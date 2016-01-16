109c3bb695e31fd1887c7fb2df016d45ef338230
Sven Michael Klose <pixel@copei.de>
2016-01-16 13:24:07 +0100

    Missing patterns.

---
+++ src/patterns.asm
@@ -0,0 +1,18 @@
+pat_empty:
+    0 0 0 0 0 0 0 0
+
+pat_solid:
+    $ff $ff $ff $ff $ff $ff $ff $ff
+
+pat_background:
+    $aa $55 $aa $55 $aa $55 $aa $55 $aa $55
+
+pat_smiley:
+    %00111100
+    %01000010
+    %10100101
+    %10100101
+    %10000001
+    %01011010
+    %00100100
+    %00011000

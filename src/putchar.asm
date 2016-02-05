a01a360c26eb660d8d6c568e708ff98de1a1aaa0
Sven Michael Klose <pixel@copei.de>
2016-02-05 01:07:17 +0100

    Clip characters at the drawing region's edges by copying a character into a buffer where parts are erased before drawing it.

--- src/putchar.asm
+++ src/putchar.asm
@@ -1,3 +1,139 @@
+buf_clipped_char:
+    fill 8
+
+masks_chars_left:
+    %11111111
+    %01111111
+    %00111111
+    %00011111
+    %00001111
+    %00000111
+    %00000011
+    %00000001
+
+masks_chars_right:
+    %00000000
+    %10000000
+    %11000000
+    %11100000
+    %11110000
+    %11111000
+    %11111100
+    %11111110
+    %11111111
+
+blit_char:
+    ; Calculate sub–column shifts.
+    lda xpos
+    and #7
+    sta tmp2
+    tax
+    lda tab_neg,x
+    sta tmp3
+
+    ; Draw left (or only) column.
+    ldy #7
+l:  lda (s),y
+    ldx tmp2
+    beq +i
+m:  lsr
+    dex
+    bne -m
+i:  ora (scr),y
+    sta (scr),y
+    dey
+    bpl -l
+
+    ; Step to next column.
+    jsr inc_xcpos
+
+    ; Draw optional right column.
+    ldy #7
+l:  lda (s),y
+    ldx tmp3
+m:  asl
+    dex
+    bne -m
+    ora (scr),y
+    sta (scr),y
+    dey
+    bpl -l
+    rts
+
+clip_char:
+    ; Copy character into buffer.
+    ldy #7
+l:  lda (s),y
+    sta buf_clipped_char,y
+    dey
+    bpl -l
+
+    lda #<buf_clipped_char
+    sta s
+    lda #>buf_clipped_char
+    sta @(++ s)
+
+    ; Clip top of character.
+    lda ryt
+    sec
+    sbc ypos
+    bcc +n
+    tay
+    dey
+    lda #0
+l:  sta buf_clipped_char,y
+    dey
+    bpl -l
+n:
+
+    ; Clip bottom of character.
+    lda ypos2
+    cmp ryb
+    bcc +n
+    lda ryb
+    sec
+    sbc ypos
+    tay
+    lda #0
+l:  sta buf_clipped_char,y
+    iny
+    cpy #8
+    bne -l
+n:
+
+    lda #$ff
+    sta tmp2
+
+    ; Clip left of character.
+    lda rxl
+    sec
+    sbc xpos
+    bcc +n
+    tay
+    lda masks_chars_left,y
+    sta tmp2
+n:
+
+    ; Clip right of character.
+    lda xpos2
+    sec
+    sbc rxr
+    bcc +n
+    tay
+    lda masks_chars_right,y
+    and tmp2
+    sta tmp2
+n:
+
+    ; Run mask for sides over character.
+    ldy #7
+l:  lda tmp2
+    and buf_clipped_char,y
+    sta buf_clipped_char,y
+    dey
+    bpl -l
+    rts
+
 putchar:
     ; ASCII to PETSCII
     cmp #@(++ (char-code #\Z)))
@@ -67,43 +203,24 @@ n:
     cmp ryt
     bcc +next_char
 
-    jsr calcscr
-
-    ; Calculate sub–column shifts.
+    ; Check if character needs to get clipped.
     lda xpos
-    and #7
-    sta tmp2
-    tax
-    lda tab_neg,x
-    sta tmp3
+    cmp rxl
+    bcc +m
+    lda rxr
+    cmp xpos2
+    bcc +m
+    lda ypos
+    cmp ryt
+    bcc +m
+    lda ryb
+    cmp ypos2
+    bcs +n
+m:  jsr clip_char
+n:
 
-    ; Draw left (or only) column.
-    ldy #7
-l:  lda (s),y
-    ldx tmp2
-    beq +i
-m:  lsr
-    dex
-    bne -m
-i:  ora (scr),y
-    sta (scr),y
-    dey
-    bpl -l
-
-    ; Step to next column.
-    jsr inc_xcpos
-
-    ; Draw optional right column.
-    ldy #7
-l:  lda (s),y
-    ldx tmp3
-m:  asl
-    dex
-    bne -m
-    ora (scr),y
-    sta (scr),y
-    dey
-    bpl -l
+    jsr calcscr
+    jsr blit_char
 
     ; Update position to next character.
 next_char:

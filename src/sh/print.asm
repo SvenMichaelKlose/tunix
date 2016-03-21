c50eccb35d162c291704957c1391bc39ea9f6a98
Sven Michael Klose <pixel@copei.de>
2016-03-21 02:48:55 +0100

    No multitask in KERNAL.

--- src/sh/print.asm
+++ src/sh/print.asm
@@ -4,7 +4,9 @@ print:
 l:  ldy #0
     lda (s),y
     beq +done
-    jsr $ffd2
+    jsr take_over   ; Stop multitasking.
+    jsr $ffd2       ; Print character via KERNAL.
+    jsr release     ; Resume multitasking.
     jsr inc_s
     jmp -l
 done:

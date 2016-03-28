5c09dbc99808090bdf6cadc87e75c57b6a126c82
Sven Michael Klose <pixel@copei.de>
2016-03-28 04:59:10 +0200

    Have mixed case charset.
    Invert case before writing to screen.

--- src/core/dev-con.asm
+++ src/core/dev-con.asm
@@ -1,31 +1,68 @@
+devcon_screen_vops:
+    <devcon_read >devcon_read
+    <devcon_write_screen >devcon_write
+
 devcon_init:
+    lda #$93            ; Clear screen.
+    jsr chrout
+    lda #%11110010      ; Up/locase chars.
+    sta $9005
+
     lda #FILE_OPEN
     sta vfile_states
-    sta @(++ vfile_states)
+    lda #0
+    sta vfile_handles
+    lda #CBMDEV_SCREEN
+    sta devcon_logical_file_numbers
+    lda #<devcon_screen_vops
+    sta vfile_ops_l
+    lda #>devcon_screen_vops
+    sta vfile_ops_h
 
     ; Initialse stdout and stderr.
-    lda #0
-    ldx #3      ; Screen
-    ldy #0
+    lda #CBMDEV_SCREEN
+    ldx #CBMDEV_SCREEN  ; Screen
+    ldy #1              ; (write)
     jsr setlfs
     jmp open
 
+; X: vfile index
 devcon_read:
     pha
+    lda vfile_handles,x
+    tax
     lda devcon_logical_file_numbers,x
     tax
     jsr chkin
     pla
     jmp chrin
 
+; X: vfile index
+; A: character
+devcon_write_screen:
+    cmp #@(char-code #\A)
+    bcc +n
+    cmp #@(++ (char-code #\Z))
+    bcs +n
+    sbc #@(- (char-code #\A) (char-code #\a) 1)
+    jmp +l
+
+n:  cmp #@(char-code #\a)
+    bcc +l
+    cmp #@(++ (char-code #\z))
+    bcs +l
+    sbc #@(- (char-code #\a) (char-code #\A) 1)
+
 devcon_write:
+l:  jsr take_over
+
     pha
+    lda vfile_handles,x
+    tax
     lda devcon_logical_file_numbers,x
     tax
     jsr chkout
     pla
-    jmp chrout
+    jsr chrout
 
-devcon_close:
-    lda devcon_logical_file_numbers,x
-    jmp close
+    jmp release

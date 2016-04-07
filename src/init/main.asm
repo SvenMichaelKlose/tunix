908a67173631f0118f9a9230c57cef12c298e1a6
Sven Michael Klose <pixel@copei.de>
2016-04-07 16:56:33 +0200

    Guru meditation when shell cannot be loaded.

--- src/init/main.asm
+++ src/init/main.asm
@@ -6,10 +6,13 @@ init:
     sta @(++ s)
     lda #0
     jsr launch
+    bcs +g
 
     ; Show that we're multitasking.
 l:  inc $1e00
     jmp -l
+
+g:  jsr guru_meditation
 
 path_sh:
     @(ascii2petscii "SH") 0

2d69ccd53174d5f4dd6d649c926e788081ebb33b
Sven Michael Klose <pixel@copei.de>
2016-04-13 06:23:46 +0200

    Print something nice but Guru meditation when exiting.

--- src/init/main.asm
+++ src/init/main.asm
@@ -7,7 +7,16 @@ init:
     lda #1          ; Wait until process finishes.
     jsr launch
 
-    jsr guru_meditation
+    lda #<txt_shutdown
+    sta s
+    lda #>txt_shutdown
+    sta @(++ s)
+    jsr devcon_print_string
+
+w:  jmp -w
+
+txt_shutdown:
+    "g shut down. Have a nice day!" 0
 
 path_sh:
     @(ascii2petscii "SH") 0

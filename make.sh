c33938b665ae0cb8817b70984db32ca9c722f39d
Sven Michael Klose <pixel@copei.de>
2016-04-16 05:58:55 +0200

    Fix removal of stale files.

--- make.sh
+++ make.sh
@@ -5,6 +5,6 @@ set -e
 mkdir -pv compiled
 cd src/sh && ./make.sh && cd ../.. && cp src/sh/sh compiled
 sbcl --noinform --core bender/bender make.lisp
-@rm -v g.zip compiled/charset.lst
+rm -fv g.zip compiled/charset.lst
 cp -v README.md compiled
 cd compiled && zip -r ../g.zip *

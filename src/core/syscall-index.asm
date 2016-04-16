a87cda98881a44a47bccf8fdd2511b0a4226a87c
Sven Michael Klose <pixel@copei.de>
2016-04-16 06:00:12 +0200

    "open" and "close" point to new functions.

--- src/core/syscall-index.asm
+++ src/core/syscall-index.asm
@@ -10,13 +10,13 @@ syscall_index:
     "halt" 0 <halt >halt
     "resume" 0 <resume >resume
 
-    "open" 0 <gopen >gopen
+    "open" 0 <fs_open >fs_open
     "read" 0 <fs_read >fs_read
     "readw" 0 <readw >readw
     "readn" 0 <readn >readn
     "readm" 0 <readm >readm
     "write" 0 <fs_write >fs_write
-    "close" 0 <gclose >gclose
+    "close" 0 <fs_close >fs_close
 
     "take_over" 0 <take_over >take_over
     "release" 0 <release >release

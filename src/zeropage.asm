0bf4d0b0363006281b1ceeca9773542051cd818e
Sven Michael Klose <pixel@copei.de>
2016-05-07 20:07:15 +0200

    Switched to ca65.

--- src/zeropage.asm
+++ src/zeropage.asm
@@ -1,25 +1,23 @@
-    data
-    org 0
+.zeropage
 
-s:      0 0     ; Source pointer.
-d:      0 0     ; Destination pointer.
-c:      0 0     ; Counter.
+s:      .word 0     ; Source pointer.
+d:      .word 0     ; Destination pointer.
+c:      .word 0     ; Counter.
 
 ; Temporaries.
-tmp:    0
-tmp2:   0
-tmp3:   0
-tmp4:   0
-tmp5:   0
-tmp6:   0
+tmp:    .byte 0
+tmp2:   .byte 0
+tmp3:   .byte 0
+tmp4:   .byte 0
+tmp5:   .byte 0
+tmp6:   .byte 0
 scr:
-tmp7:   0
-tmp8:   0
+tmp7:   .byte 0
+tmp8:   .byte 0
 
-do_load_library:       0
-do_make_jumps_to_core: 0
+do_load_library:       .byte 0
+do_make_jumps_to_core: .byte 0
 
-last_error: 0
+last_error: .byte 0
 
 zp_end_core:
-    end

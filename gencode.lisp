f4d6fb100b66c1660b104575af0dbbd3304efcbe
Sven Michael Klose <pixel@copei.de>
2016-05-08 10:59:35 +0200

    Moved to GNU Make.

--- gencode.lisp
+++ gencode.lisp
@@ -211,11 +211,11 @@
 			(syscall-args))
          o))
 (with-output-file o "src/lib/gfx/_column-addrs.asm"
-  (princ (+ (format nil ".export column_addrs_l, column_addrs_h~%")
-			"column_addrs_l: .byte "
+  (princ (+ (format nil ".export column_addrs_lo, column_addrs_hi~%.data~%")
+			"column_addrs_lo: .byte "
 			(apply #'+ (pad (maptimes [princ (low (+ #x1100 (* 16 12 _))) nil] 20) ", "))
 			(format nil "~%")
-  			"column_addrs_h: .byte "
+  			"column_addrs_hi: .byte "
 			(apply #'+ (pad (maptimes [princ (high (+ #x1100 (* 16 12 _))) nil] 20) ", "))
 			(format nil "~%"))
 		 o))
--- src/lib/gfx/_column-addrs.asm
+++ src/lib/gfx/_column-addrs.asm
@@ -1,3 +1,4 @@
-.export column_addrs_l, column_addrs_h
-column_addrs_l: .byte 0, 192, 128, 64, 0, 192, 128, 64, 0, 192, 128, 64, 0, 192, 128, 64, 0, 192, 128, 64
-column_addrs_h: .byte 17, 17, 18, 19, 20, 20, 21, 22, 23, 23, 24, 25, 26, 26, 27, 28, 29, 29, 30, 31
+.export column_addrs_lo, column_addrs_hi
+.data
+column_addrs_lo: .byte 0, 192, 128, 64, 0, 192, 128, 64, 0, 192, 128, 64, 0, 192, 128, 64, 0, 192, 128, 64
+column_addrs_hi: .byte 17, 17, 18, 19, 20, 20, 21, 22, 23, 23, 24, 25, 26, 26, 27, 28, 29, 29, 30, 31

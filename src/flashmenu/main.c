40ed79ec10aa70c97ca1d69420cf94db530061a7
Sven Michael Klose <pixel@copei.de>
2016-05-08 19:39:42 +0200

    We'll start with a BASIC start window.

--- src/flashmenu/main.c
+++ src/flashmenu/main.c
@@ -53,34 +53,54 @@ void seek_and_load_config ()
 }
 
 short devices;
+struct window * wleft;
+struct window * wright;
 
 void
 init_filewindow (struct window * w, char device)
 {
+
     /* Make window. */
     /* Draw window. */
     /* Load directory listing. */
     /* Draw window content. */
+
+	wleft = make_window (0, 0, 81, 176, "#8");
+	draw_window (wleft);
+    wright = make_window (80, 0, 80, 176, "#1");
+    draw_window (wright);
+}
+
+void
+draw_background ()
+{
+    gfx_reset_region ();
+    gfx_set_pattern (pattern_gray);
+    gfx_draw_box (0, 0, 20 * 8, 12 * 16);
+}
+
+void
+win_basic_start ()
+{
+	struct window * win = make_window (0, 0, 160, 176, "Start BASIC...");
+
+	draw_window (win);
+    gfx_set_pattern (pattern_solid);
+    gfx_draw_text (2, 50, "( ) no extra memory");
+    gfx_draw_text (2, 60, "[ ] +3K");
+    gfx_draw_text (2, 70, "( ) +8K");
+    gfx_draw_text (2, 80, "( ) +16K");
+    gfx_draw_text (2, 90, "( ) +24K");
 }
 
 int
 main (int argc, char ** argv)
 {
-	struct window * wleft;
-	struct window * wright;
-
     gfx_init ();
-    gfx_clear_screen (WHITE);
-    gfx_reset_region ();
-    gfx_set_pattern (pattern_gray);
-    gfx_draw_box (0, 0, 20 * 8, 12 * 16);
+    draw_background ();
+    win_basic_start ();
 
-	wleft = make_window (0, 0, 81, 176, "#8");
-	draw_window (wleft);
-	wright = make_window (80, 0, 80, 176, "#1");
-	draw_window (wright);
-
-	while (1);
+    while (1);
     probe_devices ();
     seek_and_load_config ();
 

bb9b221280ee70c212306c6a8bcd17cee84907ce
Sven Michael Klose <pixel@copei.de>
2016-05-09 11:34:55 +0200

    Print message when running out of heap memory.

--- src/flashmenu/window.c
+++ src/flashmenu/window.c
@@ -9,6 +9,11 @@
 void
 errnomem ()
 {
+    gfx_clear_screen (1);
+    gfx_set_font (charset_4x8);
+    gfx_set_pencil_mode (1);
+    gfx_draw_text (0, 84, "Error: Out of heap memory.");
+    while (1);
 }
 
 struct window * __fastcall__

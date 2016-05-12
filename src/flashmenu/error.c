983f327df50fab4d3d69d49da1b931e36eb8fe77
Sven Michael Klose <pixel@copei.de>
2016-05-12 16:18:14 +0200

    Print error message on top of screen without clearing it.

--- src/flashmenu/error.c
+++ src/flashmenu/error.c
@@ -3,10 +3,12 @@
 void
 print_error (char * text)
 {
-    gfx_clear_screen (1);
     gfx_set_font (charset_4x8, 2);
     gfx_set_pencil_mode (1);
-    gfx_draw_text (0, 84, text);
+    gfx_set_pattern (pattern_empty);
+    gfx_draw_box (0, 0, 160, 8);
+    gfx_set_pattern (pattern_solid);
+    gfx_draw_text (0, 0, text);
     while (1);
 }
 

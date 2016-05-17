f249a95cc95bf9bda0db3fe41b0d9b5b3eb3336b
Sven Michael Klose <pixel@copei.de>
2016-05-17 10:22:52 +0200

    print_obj(): New function prints the 'rect' of an obj and the current region.

--- src/flashmenu/message.c
+++ src/flashmenu/message.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+
 #include "libgfx.h"
+#include "obj.h"
 #include "message.h"
 
+gpos my = 0;
 void
 print_message (char * text)
 {
@@ -9,8 +13,20 @@ print_message (char * text)
     gfx_set_font (charset_4x8, 2);
     gfx_set_pencil_mode (1);
     gfx_set_pattern (pattern_empty);
-    gfx_draw_box (0, 23 * 8, 160, 8);
+    gfx_draw_box (0, my, 160, 8);
     gfx_set_pattern (pattern_solid);
-    gfx_draw_text (0, 23 * 8, text);
+    gfx_draw_text (0, my, text);
     gfx_pop_region ();
+    my += 8;
+}
+
+char msg[256];
+
+void
+print_obj (struct obj * o)
+{
+    sprintf (msg, "cd: %d %d %d %d - r: %d %d %d %d",
+                  o->rect.x, o->rect.y, o->rect.w, o->rect.h,
+                  gfx_rxl (), gfx_ryt (), gfx_rxr (), gfx_ryb ());
+    print_message (msg);
 }
--- src/flashmenu/message.h
+++ src/flashmenu/message.h
@@ -2,5 +2,6 @@
 #define MESSAGE_H
 
 void print_message (char *);
+void print_obj (struct obj *);
 
 #endif /* #ifndef MESSAGE_H */

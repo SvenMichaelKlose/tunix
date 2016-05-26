90fb1f6bf0d46216f60b91341617bdd05bd83f2a
Sven Michael Klose <pixel@copei.de>
2016-05-26 06:35:09 +0200

    Cosmetic update – draw hline above text.

--- src/flashmenu/message.c
+++ src/flashmenu/message.c
@@ -12,8 +12,9 @@ print_message (char * text)
     gfx_set_font (charset_4x8, 2);
     gfx_set_pencil_mode (1);
     gfx_set_pattern (pattern_empty);
-    gfx_draw_box (0, 23 * 8, 160, 8);
+    gfx_draw_box (0, 23 * 8 - 1, 160, 9);
     gfx_set_pattern (pattern_solid);
+    gfx_draw_hline (0, 23 * 8 - 2, 20 * 8);
     gfx_draw_text (0, 23 * 8, text);
     gfx_pop_context ();
 }

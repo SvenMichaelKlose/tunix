69c30f087cc3a5cfb75ca4e22277ba320576b711
Sven Michael Klose <pixel@copei.de>
2016-05-26 06:22:05 +0200

    Draw message at the bottom of the screen.

--- src/flashmenu/message.c
+++ src/flashmenu/message.c
@@ -12,9 +12,9 @@ print_message (char * text)
     gfx_set_font (charset_4x8, 2);
     gfx_set_pencil_mode (1);
     gfx_set_pattern (pattern_empty);
-    gfx_draw_box (0, 0, 160, 8);
+    gfx_draw_box (0, 23 * 8, 160, 8);
     gfx_set_pattern (pattern_solid);
-    gfx_draw_text (0, 0, text);
+    gfx_draw_text (0, 23 * 8, text);
     gfx_pop_context ();
 }
 

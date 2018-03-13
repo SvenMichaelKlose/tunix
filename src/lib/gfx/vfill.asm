acfca75d9b448c9889be33626d000118789361d2
Sven Michael Klose <pixel@copei.de>
2018-03-13 02:52:23 +0100

    Align pattern to screen.

--- src/lib/gfx/vfill.asm
+++ src/lib/gfx/vfill.asm
@@ -1,12 +1,12 @@
 .export vfill, vcopy
 
-.importzp scr, pattern, height, masks, maskd, tmp
+.importzp scr, pattern, ypos, height, masks, maskd, tmp
 
 .code
 
 ; SYSCALL: Fill part of column
 ;
-; In:   height, pattern
+; In:   ypos, height, pattern
 ;       masks:  Source mask (ANDed with pattern).
 ;       maskd:  Destination mask (ANDed with screen).
 ;       scr:    Starting address.
@@ -15,11 +15,11 @@
     sta mod_pattern+1
     lda pattern+1
     sta mod_pattern+2
-    ldy height
-l:  tya
+    lda ypos
     and #7
     tax
-    lda (scr),y
+    ldy height
+l:  lda (scr),y
     and maskd
     sta tmp
 mod_pattern:
@@ -27,24 +27,35 @@ mod_pattern:
     and masks
     ora tmp
     sta (scr),y
+    inx
+    txa
+    and #7
+    tax
     dey
     bne l
 
     rts
 .endproc
 
+; In:   ypos, height, pattern
+;       scr:    Starting address.
 .proc vcopy
     lda pattern
     sta mod_pattern+1
     lda pattern+1
     sta mod_pattern+2
-    ldy height
-l:  tya
+    lda ypos
     and #7
     tax
+    ldy height
+l:  
 mod_pattern:
     lda $ffff,x
     sta (scr),y
+    inx
+    txa
+    and #7
+    tax
     dey
     bne l
 

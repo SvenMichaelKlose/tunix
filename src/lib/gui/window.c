4dcfc7db7fd58f37faf0a5ec7cb7cfb9d25b38c7
Sven Michael Klose <pixel@copei.de>
2018-09-14 20:21:16 +0200

    FUll-screen mode added.

--- src/lib/gui/window.c
+++ src/lib/gui/window.c
@@ -28,9 +28,58 @@ make_window (char * title)
 }
 
 void __fastcall__
-draw_window (struct obj * _w)
+draw_window_fullscreen (struct window * win)
 {
-    struct window * win = (struct window *) _w;
+    struct rect * r = &win->obj.rect;
+    gpos   x = 0;
+    gpos   y = WINDOW_TITLE_HEIGHT - 1;
+    gsize  w = 160;
+    gsize  h = 183;
+    gsize  iw = r->w - 2;
+    gpos   yb = WINDOW_TITLE_HEIGHT - 3;
+    gpos   cx;
+    gpos   ch = h - WINDOW_TITLE_HEIGHT;
+    gsize  gw;
+    gpos   y2;
+
+    /* Draw window title. */
+    gfx_push_context ();
+    gfx_reset_region ();
+    gfx_set_region (0, 0, w, WINDOW_TITLE_HEIGHT);
+    gfx_set_pencil_mode (PENCIL_MODE_OR);
+    gfx_set_pattern (pattern_empty);
+    gfx_draw_box (0, 0, w, WINDOW_TITLE_HEIGHT - 2);
+    gfx_set_pattern (pattern_solid);
+    gfx_draw_hline (0, WINDOW_TITLE_HEIGHT - 2, w);
+    gfx_set_font (charset_4x8, 2);
+    gfx_draw_text (1, 1, win->title);
+
+    /* Draw title grip. */
+    cx = gfx_x ();
+    if (cx <= iw) {
+        gw = iw - cx;
+        for (y2 = 1; y2 < yb; y2 += 2)
+            gfx_draw_hline (cx, y2, gw);
+    }
+    gfx_pop_context ();
+
+    /* Draw content frame. */
+    gfx_push_context ();
+
+    /* Clear content area. */
+    gfx_set_region (0, WINDOW_TITLE_HEIGHT - 1, w, ch);
+    gfx_set_pattern (pattern_empty);
+    gfx_draw_box (0, 0, w, ch);
+
+    /* Draw contents. */
+    draw_obj_children (OBJ(win));
+    gfx_pop_context ();
+}
+
+
+void __fastcall__
+draw_window_standard (struct window * win)
+{
     struct rect * r = &win->obj.rect;
     gpos   x = r->x;
     gpos   y = r->y;
@@ -69,8 +118,10 @@ draw_window (struct obj * _w)
     gfx_set_pattern (pattern_solid);
     gfx_set_region (x, y + WINDOW_TITLE_HEIGHT - 1, w, ch);
     gfx_draw_frame (0, 0, w, h - WINDOW_TITLE_HEIGHT);
+    gfx_pop_context ();
 
     /* Clear content area. */
+    gfx_push_context ();
     gfx_set_region (x + 1, y + WINDOW_TITLE_HEIGHT, iw, ch);
     gfx_set_pattern (pattern_empty);
     gfx_draw_box (0, 0, iw, ch - 1);
@@ -78,6 +129,17 @@ draw_window (struct obj * _w)
     /* Draw contents. */
     draw_obj_children (OBJ(win));
     gfx_pop_context ();
+}
+
+void __fastcall__
+draw_window (struct obj * _w)
+{
+    struct window * win = (struct window *) _w;
+
+    if (win->flags & W_FULLSCREEN)
+        draw_window_fullscreen (win);
+    else
+        draw_window_standard (win);
 }
 
 void
--- src/lib/gui/window.h
+++ src/lib/gui/window.h
@@ -3,9 +3,12 @@
 
 extern struct obj_ops window_ops;
 
+#define W_FULLSCREEN    1
+
 struct window {
     struct obj  obj;
     char *      title;
+    char        flags;
 };
 
 struct window * __fastcall__ make_window (char * title);

eb029bd1c69fb2a0be32799a31a2fcae7de1a81e
Sven Michael Klose <pixel@copei.de>
2018-09-24 15:45:22 +0200

    Fix type.

--- mkfs/ultifs.h
+++ mkfs/ultifs.h
@@ -14,7 +14,7 @@ typedef unsigned long usize;
 struct _bfile {
     upos    start;          /* Start of file data. */
     upos    ptr;            /* Current position in file data. */
-    upos    size;           /* Current size (grows on writes). */
+    usize   size;           /* Current size (grows on writes). */
     upos    directory;      /* The directory this file is in. */
     upos    replaced;       /* Position of block this one replaced. Yet unused? */
     char    mode;           /* Mode at bfile_open(). */

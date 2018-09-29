c61ab59470d99ac69cbe910af312a5a23b334285
Sven Michael Klose <pixel@copei.de>
2018-09-29 19:44:35 +0200

    Added missing files for overlays.

---
+++ src/flashmenu/wrap-ultifs.c
@@ -0,0 +1,89 @@
+#include <cbm.h>
+
+#include <ultimem-basics.h>
+#include "ultifs.h"
+#include "wrap-ultifs.h"
+
+#define ULTIFS_BANK     6
+
+bfile * __fastcall__
+w_ultifs_open (upos directory, char * name, char mode)
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    bfile * r;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    r = ultifs_open (directory, name, mode);
+    *ULTIMEM_BLK1 = oldbank;
+    return r;
+}
+
+int __fastcall__
+w_bfile_readm (bfile * b, char * bytes, unsigned len)
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    int r;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    r = bfile_readm (b, bytes, len);
+    *ULTIMEM_BLK1 = oldbank;
+    return r;
+}
+
+void __fastcall__
+w_bfile_close (bfile * b)
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    bfile_close (b);
+    *ULTIMEM_BLK1 = oldbank;
+}
+
+char
+w_ultifs_opendir ()
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    char r;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    r = ultifs_opendir ();
+    *ULTIMEM_BLK1 = oldbank;
+    return r;
+}
+
+char __fastcall__
+w_ultifs_readdir (struct cbm_dirent * dirent)
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    char r;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    r = ultifs_readdir (dirent);
+    *ULTIMEM_BLK1 = oldbank;
+    return r;
+}
+
+void
+w_ultifs_closedir ()
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    ultifs_closedir ();
+    *ULTIMEM_BLK1 = oldbank;
+}
+
+char __fastcall__
+w_ultifs_enterdir (char * name)
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    char r;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    r = ultifs_enterdir (name);
+    *ULTIMEM_BLK1 = oldbank;
+    return r;
+}
+
+void
+w_ultifs_leavedir ()
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    *ULTIMEM_BLK1 = ULTIFS_BANK;
+    ultifs_leavedir ();
+    *ULTIMEM_BLK1 = oldbank;
+}
---
+++ src/flashmenu/wrap-ultifs.h
@@ -0,0 +1,14 @@
+#ifndef WRAP_ULTIFS_H
+#define WRAP_ULTIFS_H
+
+extern bfile * __fastcall__ w_ultifs_open (upos directory, char * name, char mode);
+extern int     __fastcall__ w_bfile_readm (bfile * b, char * bytes, unsigned len);
+extern void    __fastcall__ w_bfile_close (bfile * b);
+
+extern char    __fastcall__ w_ultifs_opendir (void);
+extern char    __fastcall__ w_ultifs_readdir (struct cbm_dirent *);
+extern void                 w_ultifs_closedir (void);
+extern char    __fastcall__ w_ultifs_enterdir (char * name);
+extern void                 w_ultifs_leavedir (void);
+
+#endif // #ifndef WRAP_ULTIFS_H

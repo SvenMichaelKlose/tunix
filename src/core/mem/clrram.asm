98108dfca72b66aa3b9d6a4a50be622f9a1eb7a9
Sven Michael Klose <pixel@copei.de>
2018-09-29 16:29:29 +0200

    Fixed countdown.

--- src/core/mem/clrram.asm
+++ src/core/mem/clrram.asm
@@ -3,19 +3,26 @@
 .importzp d, c
 
 .proc clrram
+    lda c
+    bne +l2
+    dec c+1
+l2: dec c
+
     ldy d
     ldx c
-    inx
     lda #0
     sta d
     sta c
-    inc c+1
-l:  sta (d),y
+l:  lda #0
+    sta (d),y
     iny
     beq m
 n:  dex
+    cpx #255
     bne l
     dec c+1
+    lda c+1
+    cmp #255
     bne l
     rts
 

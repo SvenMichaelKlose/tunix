ea616d8df0e069665309cd8804e5bc4d8c53e86e
Sven Michael Klose <pixel@copei.de>
2018-10-28 13:19:31 +0100

    Erase flash ROM.

--- src/installer/main.asm
+++ src/installer/main.asm
@@ -1,10 +1,23 @@
 .export _main
+.import _ultimem_erase_chip
+.import _ultimem_burn_byte
+.import pushax
+.importzp s
 
 .code
 
 .proc _main
     lda #<txt_welcome
     ldy #>txt_welcome
+    jsr $cb1e
+
+    jsr _ultimem_erase_chip
+    lda #<txt_chip_erased
+    ldy #>txt_chip_erased
+    jsr $cb1e
+
+    lda #<txt_installing
+    ldy #>txt_installing
     jsr $cb1e
 
     lda #2
@@ -19,12 +32,24 @@
     ldx #2
     jsr $ffc6
 
+    lda #$00
+    sta s
+    lda #$a0
+    sta s+1
+
 l:  jsr $ffcf
     pha
     lda $90
     cmp #1
     pla
     bcc l
+
+    pha
+    lda s
+    ldx s+1
+    jsr pushax
+    pla
+    jsr _ultimem_burn_byte
 
     jsr $ffcc
     lda #2
@@ -34,6 +59,12 @@ l:  jsr $ffcf
 
 txt_welcome:
     .byte $93, "G INSTALLER", 13, 0
+
+txt_installing:
+    .byte $93, "INSTALLING G", 13, 0
+
+txt_chip_erased:
+    .byte "FLASH ROM ERASED.", 13,0
 
 fn_data:
     .byte "GDATA.BIN,S,R"

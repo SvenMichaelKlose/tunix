d9ae8c7df9929f5f77ee7c4c7b32da553f7f408b
Sven Michael Klose <pixel@copei.de>
2018-10-29 12:44:24 +0100

    Missed out on adding files.

---
+++ src/desktop/wrap-file-window.c
@@ -0,0 +1,19 @@
+#include <cbm.h>
+
+#include <ultimem-basics.h>
+
+#include "obj.h"
+#include "ultifs.h"
+#include "file-window.h"
+
+struct obj * __fastcall__ w_make_file_window (struct drive_ops * ops, char * title, gpos x, gpos y, gsize w, gsize h)
+{
+    unsigned oldbank = *ULTIMEM_BLK1;
+    struct obj * r;
+
+    *ULTIMEM_BLK1 = FILE_WINDOW_BANK;
+    r = make_file_window (ops, title, x, y, w, h);
+    *ULTIMEM_BLK1 = oldbank;
+
+    return r;
+}
---
+++ src/installer/zeropage.asm
@@ -0,0 +1,10 @@
+.exportzp s, d, c, tmp
+
+.zeropage
+
+s = 0
+d = 2
+c = 4
+
+; Temporaries.
+tmp = 6

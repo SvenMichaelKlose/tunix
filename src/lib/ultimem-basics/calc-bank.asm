e523e62560195ebd3b067684167ba95e022b753b
Sven Michael Klose <pixel@hugbox.org>
2019-05-19 14:43:55 +0200

    Moved flashboot bank copy routine here.

---
+++ src/lib/ultimem-basics/calc-bank.asm
@@ -0,0 +1,107 @@
+.export ultimem_read_byte
+.export ultimem_write_byte
+.export ultimem_get_bank
+.exportzp bp
+.importzp s, d, c, tmp
+
+.zeropage
+
+bp:     .res 4
+
+.code
+
+.proc ultimem_get_bank
+    lda 1,x
+    lsr
+    lsr
+    lsr
+    lsr
+    lsr
+    sta $9ff0,y
+    lda 2,x
+    asl
+    asl
+    asl
+    ora $9ff0,y
+    sta $9ff0,y
+    lda 2,x
+    lsr
+    lsr
+    lsr
+    lsr
+    lsr
+    sta $9ff1,y
+    lda 0,x
+    sta s
+    lda 1,x
+    and #%00011111
+    sta s+1
+    ldy #0
+    rts
+.endproc
+
+; Fetch byte from Flash memory at 24-bit offset in 0,X.
+.proc ultimem_read_byte
+    tya
+    pha
+    lda $9ff2
+    pha
+    lda #%01111101
+    sta $9ff2
+    lda $9ff8
+    pha
+    lda $9ff9
+    pha
+
+    ldy #8
+    jsr ultimem_get_bank
+    lda s+1
+    ora #$20
+    sta s+1
+    lda (s),y
+    sta tmp
+
+    pla
+    sta $9ff9
+    pla
+    sta $9ff8
+    pla
+    sta $9ff2
+    pla
+    tay
+    lda tmp
+    rts
+.endproc
+
+; Write byte to RAM at 24-bit offset in 0,X.
+.proc ultimem_write_byte
+    sta tmp
+    tya
+    pha
+    lda $9ff2
+    pha
+    lda #%01111111
+    sta $9ff2
+    lda $9ff8
+    pha
+    lda $9ff9
+    pha
+
+    ldy #8
+    jsr ultimem_get_bank
+    lda s+1
+    ora #$20
+    sta s+1
+    lda tmp
+    sta (s),y
+
+    pla
+    sta $9ff9
+    pla
+    sta $9ff8
+    pla
+    sta $9ff2
+    pla
+    tay
+    rts
+.endproc
--- src/lib/ultimem-basics/copy-rom2ram.asm
+++ src/lib/ultimem-basics/copy-rom2ram.asm
@@ -1,18 +1,22 @@
 .export ultimem_copy_rom2ram
+.export ultimem_copy_ram2rom
 .importzp s, d, c, base, size, ptr
 
-.import ultimem_read_byte
-.import ultimem_write_byte
 .import ultimem_get_bank
-.import copyd
 
 .code
 
-block_size = 0
-block_replacement = 4
-block_next = 8
-block_type = 12
-block_namelen = 13
+.proc copyd
+    lda 0,x
+    sta 0,y
+    lda 1,x
+    sta 1,y
+    lda 2,x
+    sta 2,y
+    lda 3,x
+    sta 3,y
+    rts
+.endproc
 
 .proc ultimem_copy_rom2ram
     lda $9ff2
@@ -27,6 +31,92 @@ block_namelen = 13
     pha
 
     lda #%01111101  ; ROMRAMRAMROM…
+    sta $9ff2
+
+    ldx #ptr
+    ldy #base
+    jsr copyd
+
+    ldx #base
+    ldy #$08
+    jsr ultimem_get_bank
+    lda s
+    sta base
+    lda s+1
+    ora #$20
+    sta base+1
+
+    ldx #d
+    ldy #$0a
+    jsr ultimem_get_bank
+    lda s
+    sta d
+    lda s+1
+    ora #$40
+    sta d+1
+
+l3: ldy #0
+    lda (base),y
+    sta (d),y
+    inc base
+    bne l4
+    inc base+1
+    lda base+1
+    cmp #$40
+    bne l4
+    lda #$20
+    sta base+1
+    inc $9ff8
+    bne l4
+    inc $9ff9
+
+l4: inc d
+    bne l5
+    inc d+1
+    lda d+1
+    cmp #$60
+    bne l5
+    lda #$40
+    sta d+1
+    inc $9ffa
+    bne l5
+    inc $9ffb
+
+l5: dec size
+    lda size
+    cmp #255
+    bne l3
+    dec size+1
+    lda size+1
+    cmp #255
+    bne l3
+
+    pla
+    sta $9ffb
+    pla
+    sta $9ffa
+    pla
+    sta $9ff9
+    pla
+    sta $9ff8
+    pla
+    sta $9ff2
+    rts
+.endproc
+
+.proc ultimem_copy_ram2rom
+    lda $9ff2
+    pha
+    lda $9ff8
+    pha
+    lda $9ff9
+    pha
+    lda $9ffa
+    pha
+    lda $9ffb
+    pha
+
+    lda #%01110101  ; ROMRAMROMROM…
     sta $9ff2
 
     ldx #ptr

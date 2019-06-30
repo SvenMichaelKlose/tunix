4512e465541b45ac32cf79f52e659d671ebf0c42
Sven Michael Klose <pixel@hugbox.org>
2019-06-30 13:32:17 +0200

    Add missing.

---
+++ src/lib/ingle/ingle.h
@@ -0,0 +1,7 @@
+#ifndef INGLE_H
+#define INGLE_H
+
+extern void __fastcall__ save_state (unsigned int restart_addr);
+extern void __fastcall__ launch (unsigned start, unsigned size);
+
+#endif /* #ifndef INGLE_H */

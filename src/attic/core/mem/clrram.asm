8a5d4585aa9254b02bbd629205ae04e93537420c
Sven Michael Klose <pixel@hugbox.org>
2019-07-08 04:02:38 +0200

    Use the countdown speedup trick again.

--- src/attic/core/mem/clrram.asm
+++ src/attic/core/mem/clrram.asm
@@ -3,26 +3,19 @@
 .importzp d, c
 
 .proc clrram
-    lda c
-    bne +l2
-    dec c+1
-l2: dec c
-
-    ldy d
     ldx c
+    inx
+    inc c+1
+    ldy d
     lda #0
     sta d
-    sta c
-l:  lda #0
-    sta (d),y
+    beq n
+l:  sta (d),y
     iny
     beq m
 n:  dex
-    cpx #255
     bne l
     dec c+1
-    lda c+1
-    cmp #255
     bne l
     rts
 

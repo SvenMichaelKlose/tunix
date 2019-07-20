ddec447704e859e319a5b30a5bb661ea0d951f4b
Sven Michael Klose <pixel@hugbox.org>
2019-07-20 02:48:41 +0200

    Minor cleanup.

--- src/lib/gui/obj.h
+++ src/lib/gui/obj.h
@@ -19,11 +19,11 @@ struct rect {
 
 // Node of tree of doubly–linked lists.
 struct node {
-    char            type;       // Yet unused.
-    struct obj *    prev;
-    struct obj *    next;
-    struct obj *    parent;
-    struct obj *    children;   // Head node of list.
+    char          type;      // Yet unused.
+    struct obj *  prev;
+    struct obj *  next;
+    struct obj *  parent;
+    struct obj *  children;  // Head node of list.
 };
 
 typedef void __fastcall__ (*func_draw_t) (struct obj *);
@@ -31,23 +31,23 @@ typedef void __fastcall__ (*func_layout_t) (struct obj *);
 typedef void __fastcall__ (*func_free_t) (struct obj *);
 
 struct obj_ops {
-    func_draw_t         draw;           // Draws object.
-    func_layout_t       layout;         // Layouts object before drawing.
-    func_free_t         free;           // Removes object resources.
-    event_handler_t     event_handler;
-    unsigned short      draw_bank;
-    unsigned short      layout_bank;
-    unsigned short      free_bank;
-    unsigned short      event_handler_bank;
+    func_draw_t      draw;           // Draws object.
+    func_layout_t    layout;         // Layouts object before drawing.
+    func_free_t      free;           // Removes object resources.
+    event_handler_t  event_handler;
+    unsigned short   draw_bank;
+    unsigned short   layout_bank;
+    unsigned short   free_bank;
+    unsigned short   event_handler_bank;
 };
 
 extern void __fastcall__ obj_noop (struct obj *);
 
 // The object. This must be the first element in derived objects.
 struct obj {
-    struct node         node;
-    struct rect         rect;
-    struct obj_ops *    ops;
+    struct node       node;
+    struct rect       rect;
+    struct obj_ops *  ops;
 };
 
 // Derived objects need to be cast. Therefore this convenience macro.
@@ -60,7 +60,7 @@ extern void __fastcall__ set_obj_position (struct obj *, gpos x, gpos y);
 extern void __fastcall__ set_obj_position_and_size (struct obj *, gpos x, gpos y, gsize w, gsize h);
 
 // Append new object to list of children.
-extern struct obj * __fastcall__ append_obj (struct obj * parent, struct obj * x);
+extern struct obj * __fastcall__ append_obj (struct obj * parent, struct obj *);
 
 // Copy obj_ops structure most probably to replace some of its functions and
 // reassign the new structure with set_obj_ops().

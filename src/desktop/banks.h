5b68a5483bafd9d0b60ba938f8b5e827b1fa493a
Sven Michael Klose <pixel@hugbox.org>
2020-04-16 06:06:20 +0200

    Add missing file.

---
+++ src/desktop/banks.h
@@ -0,0 +1,9 @@
+#ifndef BANKS_H
+#define BANKS_H
+
+#define DESKTOP_BANK        1
+#define ULTIFS_BANK         5
+#define FONT_BANK           6
+#define FILE_WINDOW_BANK    7
+
+#endif /* #ifndef BANKS_H */

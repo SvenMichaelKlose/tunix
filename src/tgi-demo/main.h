9a061218d25620f9aed1792bf6ce67899d6b9e12
Sven Michael Klose <pixel@hugbox.org>
2020-04-16 06:15:16 +0200

    Remvoe TGI demo.

--- src/tgi-demo/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-extern struct obj * desktop;
-extern struct obj * focussed_window;
-
-#endif /* #ifndef MAIN_H */
--- src/tgi-demo/prgend.asm
+++
@@ -1,7 +0,0 @@
-; Sven Michael Klose <pixel@hugbox.org>
-
-.export __PRGEND__
-
-.segment "PRGEND"
-
-__PRGEND__:

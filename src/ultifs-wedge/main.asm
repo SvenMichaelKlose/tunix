17c869a6f0892d13bc0386ed63b744e9dc23df9b
Sven Michael Klose <pixel@hugbox.org>
2020-09-13 09:41:57 +0200

    Preserve some thoughts.

--- src/ultifs-wedge/main.asm
+++ src/ultifs-wedge/main.asm
@@ -1,13 +1,31 @@
+; Loads UltiFS into its bank and installs primary wedge
+; for the current process.
+;
+; WANTED: The primary wedge is very small, so it fits
+; into the unused low memory area from $02a1-$2ff.
+; The user should be able to pick any position for it
+; as long as it's not in BLK1-BLK3 ($2000-$7fff).
+; Also the device number should be selectable.
+
 .export _main
 
 .forceimport __STARTUP__
 
+IOPEN   = $031A     ; KERNAL vector - open a logical file
+ICLOSE  = $031C     ; KERNAL vector - close a specified logical file
+ICHKIN  = $031E     ; KERNAL vector - open channel for input
+ICKOUT  = $0320     ; KERNAL vector - open channel for output
+ICLRCN  = $0322     ; KERNAL vector - close input and output channels
+IBASIN  = $0324     ; KERNAL vector - input character from channel
+IBSOUT  = $0326     ; KERNAL vector - output character to channel
+ICLALL  = $032C     ; KERNAL vector - close all channels and files
+ILOAD   = $0330     ; KERNAL vector - load
+ISAVE   = $0332     ; KERNAL vector - save
 
 .data
 
 txt_welcome:
     .byte $93, "ULTIFS WEDGE", 13, 0
-
 
 .code
 

f1bbca766ab6cdeb80789247c10cb82c7fa8336e
Sven Michael Klose <pixel@hugbox.org>
2020-09-13 10:04:25 +0200

    Tell about device number checking.

--- src/ultifs-wedge/wedge-secondary.asm
+++ src/ultifs-wedge/wedge-secondary.asm
@@ -1,6 +1,8 @@
 ; UltiFS-space wedge
 ;
 ; Residing in BLK1 this wraps to the UltiFS C functions.
+; If another device is being accessed, the old function
+; vector is being called.
 ;
 ; Most likely this will map in the rest of the UltiFS
 ; code the primary wedge did not care about.

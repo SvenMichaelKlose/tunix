461b363aba7bb8d13ecdc9da0b520bd6890562de
Sven Michael Klose <pixel@hugbox.org>
2020-10-24 19:47:54 +0200

    A many random changes to keep going.

--- src/ultifs-wedge/README.md
+++ src/ultifs-wedge/README.md
@@ -2,3 +2,24 @@ UltiFS wedge
 ============
 
 An attempt to hook the C version on top of the KERNAL.
+
+# How it is going to work
+
+The wedge is effectively being loaded to address $2000.
+After initialisation (C libs are being used) it copies
+itself to other RAM banks to operate from there, leaving
+only a small piece of wedge code that will divert KERNAL
+calls for the next program on exit.
+
+This "primary wedge" is as small as can be.  It only
+banks in the secondary wedge which will finish banking
+in the rest of the UltiFS to then finally call the
+KERNAL emulating code.  On return, the secondary wedge
+brings most of the calling program back in and leaves
+the rest to the primary wedge.
+
+This overhead has two advantages.  First, only very few
+bytes of the original program space needs to be occupied.
+It even fits into unused low memory areas.  Second,
+complicated file system code does not have to be written
+in assembly.
--- src/ultifs-wedge/wedge-secondary.asm
+++ src/ultifs-wedge/wedge-secondary.asm
@@ -12,15 +12,65 @@
 
 .export _blk2
 
-.import _ultifs_open
+.import _ultifs_kopen
+.import _ultifs_kclose
+.import _ultifs_kchkin
+.import _ultifs_kchkout
+.import _ultifs_kclrcn
+.import _ultifs_kbasin
+.import _ultifs_kbasout
+.import _ultifs_kclall
+
 .import __ZP_START__
 .import __ZP_SIZE__
 
 .segment "SECONDARY"
 
+FA      = $ba   ; Device number
+
+IOPEN   = $031A     ; KERNAL vector - open a logical file
+ICLOSE  = $031C     ; KERNAL vector - close a specified logical file
+ICHKIN  = $031E     ; KERNAL vector - open channel for input
+ICKOUT  = $0320     ; KERNAL vector - open channel for output
+ICLRCN  = $0322     ; KERNAL vector - close input and output channels IBASIN  = $0324     ; KERNAL vector - input character from channel IBSOUT  = $0326     ; KERNAL vector - output character to channel
+ICLALL  = $032C     ; KERNAL vector - close all channels and files
+ILOAD   = $0330     ; KERNAL vector - load
+ISAVE   = $0332     ; KERNAL vector - save
+
 _blk2:      .res 1
+_udevs:     .res 1
+
+old_IOPEN:      .res 2
+old_ICLOSE:     .res 2
+old_ICHKIN:     .res 2
+old_ICHKOUT:    .res 2
+old_ICLRCN:     .res 2
+old_IBASIN:     .res 2
+old_IBASOUT:    .res 2
+old_ICLALL:     .res 2
+
+new_vectors:
+    .word uopen
+    .word uclose
+    .word uchkin
+    .word uchkout
+    .word uclrcn
+    .word ubasin
+    .word ubasout
+    .word uclall
 
 _saved_zp:  .res $80
+
+.proc _init_kernal_vectors
+    ldx #15
+l:  lda IOPEN,x
+    sta old_IOPEN,x
+    lda new_vectors,x
+    sta IOPEN,x
+    dex
+    bpl l
+    rts
+.endproc
 
 .proc swap_zp
     ldx #$20
@@ -35,10 +85,85 @@ l:  lda __ZP_START__,x
     rts
 .endproc
 
-.proc uopen
+.proc is_our_device
+    pha
+    lda FA
+    cmp _udevs
+    pla
+.endproc
+
+.proc enter
     lda _blk2
     sta $9ffa
-    jsr swap_zp
-    jsr _ultifs_open
     jmp swap_zp
+.endproc
+
+.proc uopen
+    jsr is_our_device
+    bcc n
+    jsr enter
+    jsr _ultifs_kopen
+    jmp swap_zp
+n:  jmp (old_IOPEN)
+.endproc
+
+.proc uclose
+    jsr is_our_device
+    bcc n
+    jsr enter
+    jsr _ultifs_kclose
+    jmp swap_zp
+n:  jmp (old_ICLOSE)
+.endproc
+
+.proc uchkin
+    jsr is_our_device
+    bcc n
+    jsr enter
+    jsr _ultifs_kchkin
+    jmp swap_zp
+n:  jmp (old_ICHKIN)
+.endproc
+
+.proc uchkout
+    jsr is_our_device
+    bcc n
+    jsr enter
+    jsr _ultifs_kchkin
+    jmp swap_zp
+n:  jmp (old_ICHKOUT)
+.endproc
+
+.proc uclrcn
+    jsr is_our_device
+    bcc n
+    jsr enter
+    jsr _ultifs_kclrcn
+    jmp swap_zp
+n:  jmp (old_ICLRCN)
+.endproc
+
+.proc ubasin
+    jsr is_our_device
+    bcc n
+    jsr enter
+    jsr _ultifs_kbasin
+    jmp swap_zp
+n:  jmp (old_IBASIN)
+.endproc
+
+.proc ubasout
+    jsr is_our_device
+    bcc n
+    jsr enter
+    jsr _ultifs_kbasout
+    jmp swap_zp
+n:  jmp (old_IBASOUT)
+.endproc
+
+.proc uclall
+    jsr enter
+    jsr _ultifs_kclall
+    jsr swap_zp
+    jmp (old_ICLALL)
 .endproc

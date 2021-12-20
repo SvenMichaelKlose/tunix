46f52e0ab9aa2b4d630451a46f66a5954e3eb4a8
Sven Michael Klose <pixel@hugbox.org>
2021-12-20 03:14:25 +0100

    Add library for KERNAL helpers.

---
+++ src/lib/kernal/printstr.asm
@@ -0,0 +1,26 @@
+__VIC20__ = 1
+.include "cbm_kernal.inc"
+
+.export printstr
+
+.importzp printptr
+
+
+.code
+
+.proc printstr
+    sta printptr
+    sty printptr+1
+
+l:  ldy #0
+    lda (printptr),y
+    beq done
+    jsr BSOUT
+    inc printptr
+    bne l
+    inc printptr+1
+    bne l
+
+done:
+    rts
+.endproc

8e55b49be0aa95e7bd8e1a67010ace026a6c57e7
Sven Michael Klose <pixel@hugbox.org>
2021-12-31 01:04:37 +0100

    Add library for POSIX functions not found in cc65.

---
+++ src/lib/posix/string.h
@@ -0,0 +1,8 @@
+#ifndef _POSIX_STRING_H
+#define _POSIX_STRING_H
+
+#include <stddef.h>
+
+size_t __fastcall__ strnlen (const char * s, size_t maxlen);     /* POSIX.1-2008 */
+
+#endif // #ifndef _POSIX_STRING_H

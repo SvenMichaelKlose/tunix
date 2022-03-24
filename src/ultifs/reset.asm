bb27fef9194bc7ca4d4a89963d3a58358e1d388f
Sven Michael Klose <pixel@hugbox.org>
2022-03-24 07:52:11 +0100

    Restart BASIC properly.

--- src/ultifs/reset.asm
+++ src/ultifs/reset.asm
@@ -1,7 +1,18 @@
 .export _reset
 
 .proc _reset
-    sei
-    jsr $e5c3   ; INITVIC
-    jmp $e378   ; BASIC cold start
+    ldx #end - start
+l:  lda start,x
+    sta $9c00,x
+    dex
+    bpl l
+
+start:
+    cld
+    jsr $e518   ; Init VIC & clear screen
+    jsr $e45b   ; Set BASIC vectors
+    jsr $e3a4   ; Init zeropage
+    jsr $e404   ; Print start message
+    jmp $e381   ; Init SP & goto READY.
+end:
 .endproc

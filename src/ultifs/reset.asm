e68746830882f35b07d502504857c11170f0a21b
Sven Michael Klose <pixel@hugbox.org>
2022-03-27 19:11:11 +0200

    Add banner.

--- src/ultifs/reset.asm
+++ src/ultifs/reset.asm
@@ -1,5 +1,7 @@
 .export _reset
 
+    .code
+
 .proc _reset
     ldx #end - start
 l:  lda start,x
@@ -13,6 +15,13 @@ start:
     jsr $e45b   ; Set BASIC vectors
     jsr $e3a4   ; Init zeropage
     jsr $e404   ; Print start message
+    ldx #<msg
+    ldy #>msg
+    jsr $cb1e   ; Print start message
     jmp $e381   ; Init SP & goto READY.
 end:
 .endproc
+
+    .rodata
+
+msg:    .byte 10,13, 10, "ULTIFS ROM ON DEV 12.", 10, 13, 10, 0

7e3fe52cdb6d414144e6ac8b312462da673aebd6
Sven Michael Klose <pixel@hugbox.org>
2022-06-05 05:03:53 +0200

    The usual gardening.

--- src/ultifs/reset.asm
+++ src/ultifs/reset.asm
@@ -28,10 +28,10 @@ start:
     sta $9fff
 
     cld
-    jsr $e518   ; Init VIC & clear screen
-    jsr $e45b   ; Set BASIC vectors
-    jsr $e3a4   ; Init zeropage
-    jsr $e404   ; Print start message
-    jmp $e381   ; Init SP & goto READY.
+    jsr $e518   ; Init VIC & clear screen.
+    jsr $e45b   ; Set BASIC vectors.
+    jsr $e3a4   ; Init zero page.
+    jsr $e404   ; Print welcome message.
+    jmp $e381   ; BASIC cold start.
 end:
 .endproc

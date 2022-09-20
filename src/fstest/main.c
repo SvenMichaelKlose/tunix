f6904b4c7dfb6ffaa72ff61673162fd1e7fe736c
Sven Michael Klose <pixel@hugbox.org>
2022-09-20 09:08:22 +0200

    Run over array of tests.

--- src/fstest/main.c
+++ src/fstest/main.c
@@ -5,10 +5,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+char device = 12;
+
+void
+test_new_disk ()
+{
+    cbm_open (15, device, 15, "N0:TESTDISK,01");
+}
+
+typedef void (*voidfun) ();
+
+typedef struct _test {
+    const char *  description;
+    voidfun       fun;
+} test;
+
+test tests[] = {
+    {"New disk", test_new_disk}
+};
+
 void
 main (void)
 {
-    char device = 12;
+    test * p;
+    int i;
+    int num_tests = sizeof (tests) / sizeof (test);
 
-    printf ("Hello world!");
+    printf ("Filesystem tests: %d\n", num_tests);
+
+    for (i = 0; i < num_tests; i++) {
+        p = &tests[i];
+        printf ("Test %d: %s\n", i + 1, p->description);
+        p->fun ();
+    }
 }

a730b9b0062af46842c5fbbba8175d3ce27581de
Sven Michael Klose <pixel@hugbox.org>
2022-09-20 08:15:20 +0200

    Add basis for implementing filesystem tests.

---
+++ src/fstest/main.c
@@ -0,0 +1,14 @@
+#include <cbm.h>
+#include <conio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void
+main (void)
+{
+    char device = 12;
+
+    printf ("Hello world!");
+}

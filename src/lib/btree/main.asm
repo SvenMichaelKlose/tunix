cfb7d42f5a8a67f145528e517ea366fdd238e680
Sven Michael Klose <pixel@hugbox.org>
2022-11-19 12:33:48 +0100

    Do not use '.proc' directive.

--- src/lib/btree/main.asm
+++ src/lib/btree/main.asm
@@ -1,4 +1,5 @@
 .export _bt_add, _bt_lookup
+.exportzp bt_p
 
 BT_LEFT     = 0
 BT_RIGHT    = 3
@@ -12,37 +13,46 @@ r:      .res 3
 
     .code
 
-.proc bt_alloc
-.endproc
+bt_alloc:
+bt_cmp_lt:
+bt_cmp_eq:
+bt_cmp_gt:
+bt_cmp_eq_parent_left:
 
-.proc bt_cmp_lt
-.endproc
-
-.proc bt_cmp_eq
-.endproc
-
-.proc bt_cmp_gt
-.endproc
-
-.proc bt_cmp_eq_parent_left
-.endproc
+step:
+    lda (bt_p),y
+    pha
+    iny
+    lda (bt_p),y
+    pha
+    iny
+    lda (bt_p),y
+    ldy #2
+    sta (bt_p),y
+    pla
+    dey
+    sta (bt_p),y
+    pla
+    dey
+    sta (bt_p),y
+    rts
 
 ;(defmethod bnode add (k v)
 ;  (? (< k key)
 ;     (_add-left k v)
 ;     (_add-right k v)))
-.proc _bt_add
+_bt_add:
     jsr bt_cmp_lt
     bne bt_add_right
-.endproc
 
 ;(defmethod bnode _add-left (k v)
 ;  (!? left
 ;      (!.add k v)
 ;      (= left (new bnode k v this))))
 
-.proc bt_add_left
+bt_add_left:
     ldy #BT_LEFT
+bt_add_left2:
     lda (bt_p),y
     bne _bt_add
 
@@ -59,26 +69,23 @@ r:      .res 3
     iny
     pla
     sta (bt_p),y
-.endproc
 
 ;(defmethod bnode _add-right (k v)
 ;  (!? right
 ;      (!.add k v)
 ;      (= right (new bnode k v this))))
-.proc bt_add_right
+bt_add_right:
     ldy #BT_RIGHT
-    bne bt_add_left
-.endproc
+    bne bt_add_left2
 
 ;(defmethod bnode _lookup-left (k)
 ;  (!? left
 ;      (!.lookup k)))
-.proc bt_lookup_left
+bt_lookup_left:
     ldy #BT_LEFT
     lda (bt_p),y
     bne _bt_lookup
     rts
-.endproc
 
 ;(defmethod bnode lookup (k)
 ;  (? (== key k)
@@ -86,33 +93,30 @@ r:      .res 3
 ;     (? (< k key )
 ;        (_lookup-left k)
 ;        (_lookup-right k))))
-.proc _bt_lookup
+_bt_lookup:
     jsr bt_cmp_eq
-    bne r
+    beq r
     jsr bt_cmp_lt
     bne bt_lookup_left
-.endproc
 
 ;(defmethod bnode _lookup-right (k)
 ;  (!? right
 ;      (!.lookup k)))
-.proc bt_lookup_right
+bt_lookup_right:
     ldy #BT_LEFT
     lda (bt_p),y
     bne _bt_lookup
     rts
-.endproc
 
 ;(defmethod bnode get-first ()
 ;  (!? left
 ;      (!.get-first)
 ;      this))
-.proc bt_first
+bt_first:
     ldy #BT_LEFT
     lda (bt_p),y
     bne bt_first
     rts
-.endproc
 
 ;(defmethod bnode _next-trav-left (k)
 ;  (!? left
@@ -120,24 +124,22 @@ r:      .res 3
 ;      (? (> key k)
 ;         this
 ;         (_next-parent k))))
-.proc bt_next_trav_left
+bt_next_trav_left:
     ldy #BT_LEFT
     lda (bt_p),y
     bne bt_next_trav
     jsr bt_cmp_gt
     beq bt_next_parent
     rts
-.endproc
 
 ;(defmethod bnode _next-trav-right (k)
 ;  (!? right
 ;      (!._next-trav k)
 ;      (_next-parent k)))
-.proc bt_next_trav_right
+bt_next_trav_right:
     ldy #BT_RIGHT
     lda (bt_p),y
     bne bt_next_trav
-.endproc
 
 ;(defmethod bnode _next-parent (k)
 ;  (!? parent
@@ -146,26 +148,24 @@ r:      .res 3
 ;            !
 ;            (!._next-trav-right k)) ; !.key is k
 ;         (!._next-parent k))))
-.proc bt_next_parent
+bt_next_parent:
     ldy #BT_PARENT
     lda (bt_p),y
-    beq r
+    beq r2
     jsr bt_cmp_eq_parent_left
     beq bt_next_parent
     jsr bt_cmp_gt
     beq bt_next_trav_right
-r:  rts
-.endproc
+r2: rts
 
 ;(defmethod bnode _next-trav (k)
 ;  (? (<= key k)
 ;     (_next-trav-right k)
 ;     (_next-trav-left k)))
-.proc bt_next_trav
+bt_next_trav:
     jsr bt_cmp_gt
     beq bt_next_trav_right
     bne bt_next_trav_left
-.endproc
 
 ;(defmethod bnode next ()
 ;   (_next-trav key))

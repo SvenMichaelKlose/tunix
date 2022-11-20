aee681d39a8f7b1f1126252a6c693a7594202bb5
Sven Michael Klose <pixel@hugbox.org>
2022-11-20 06:26:44 +0100

    Scribble around a bit.

--- src/lib/ingle/fork.c
+++ src/lib/ingle/fork.c
@@ -3,10 +3,10 @@ fork ()
 {
     char pid;
 
-    cbm_open (31, 31, 15, "PF");
-    cbm_ckout (31);
-    pid = cbm_basin ();
-    cbm_close (31);
+    cbm_k_open (31, 31, 15, "PF");
+    cbm_k_ckout (31);
+    pid = cbm_k_basin ();
+    cbm_k_close (31);
 
     return pid;
 }

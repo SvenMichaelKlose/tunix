56c13c22adc33adb658bed9fc867c49ea7ada6be
Sven Michael Klose <pixel@hugbox.org>
2022-12-11 05:37:21 +0100

    Log to dedicated UltiMem bank (116).

--- src/ultifs/log.c
+++ src/ultifs/log.c
@@ -3,16 +3,23 @@
 #include <string.h>
 
 #include "log.h"
+#include "lib/ultimem/ultimem.h"
 
-char * log_ptr = (char *) 0x400;
+char * log_ptr = (char *) 0xa000;
+
+char log_bank = 116;
 
 void
 log_message (char * format, ...)
 {
     va_list args;
+    char oldbank = *ULTIMEM_BLK5;
+    *ULTIMEM_BLK5 = log_bank;
 
     va_start(args, format);
     vsprintf (log_ptr, format, args);
     log_ptr += strlen (log_ptr) + 1;
     va_end(args);
+
+    *ULTIMEM_BLK5 = oldbank;
 }

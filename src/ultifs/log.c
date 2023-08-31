0c44b0e3d605e9ab4c820db8b49d1907ff9e19b0
Sven Michael Klose <pixel@hugbox.org>
2023-08-31 05:37:52 +0200

    Log to RAM123 instead of BLK5.

--- src/ultifs/log.c
+++ src/ultifs/log.c
@@ -5,7 +5,7 @@
 #include "log.h"
 #include "lib/ultimem/ultimem.h"
 
-char * log_ptr = (char *) 0xa000;
+char * log_ptr = (char *) 0x0400;
 
 char log_bank = 116;
 
@@ -13,13 +13,13 @@ void
 log_message (char * format, ...)
 {
     va_list args;
-    char oldbank = *ULTIMEM_BLK5;
-    *ULTIMEM_BLK5 = log_bank;
+    //char oldbank = *ULTIMEM_BLK5;
+    //*ULTIMEM_BLK5 = log_bank;
 
     va_start(args, format);
     vsprintf (log_ptr, format, args);
     log_ptr += strlen (log_ptr) + 1;
     va_end(args);
 
-    *ULTIMEM_BLK5 = oldbank;
+    //*ULTIMEM_BLK5 = oldbank;
 }

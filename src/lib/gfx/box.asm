a6fe04094b3ebfe47d369c3990614da1699a7adf
Sven Michael Klose <pixel@hugbox.org>
2024-03-03 23:15:59 +0100

    Minor clean-up.

--- src/lib/gfx/box.asm
+++ src/lib/gfx/box.asm
@@ -1,15 +1,16 @@
 .export box
 
 .import masks_left, masks_right, maskd_left, maskd_right
-.import calcscr, dec_scr, clip_x, clip_y, add_region_position, sub_region_position, vfill, vcopy, inc_xcpos
+.import calcscr, dec_scr, clip_x, clip_y
+.import add_region_position, sub_region_position, vfill, vcopy, inc_xcpos
 .importzp c, scr, xpos, ypos, width, height, c, xcpos, pattern, masks, maskd
 
-.bss
+    .bss
 
-xposr:  .byte 0
-tmp:    .byte 0
+xposr:  .res 1
+tmp:    .res 1
 
-.code
+    .code
 
 ; In: xpos, ypos, width, height
 

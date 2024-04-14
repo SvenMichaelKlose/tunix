2c11c838be50e6c58885b22f9b0f170dfe13cf71
Sven Michael Klose <pixel@hugbox.org>
2024-04-15 00:48:45 +0200

    Add addition and comparison.

--- src/lib/zp/dword.asm
+++ src/lib/zp/dword.asm
@@ -1,9 +1,11 @@
 .export zpd_mov_xy
+.export zpd_add_xy
+.export zpd_cmp_xy
 .export zpd_inc_x
 .export zpd_addb_xay
 .export zpd_is_minus1_y
 
-.code
+    .code
 
 .proc zpd_mov_xy
     lda 0,x
@@ -15,6 +17,41 @@
     lda 3,x
     sta 3,y
     rts
+.endproc
+
+.proc zpd_add_xy
+    lda 0,x
+    clc
+    adc 0,y
+    sta 0,x
+    lda 1,x
+    adc 1,y
+    sta 1,x
+    lda 2,x
+    adc 2,y
+    sta 2,x
+    lda 3,x
+    adc 3,y
+    sta 3,x
+    rts
+.endproc
+
+.proc zpd_cmp_xy
+    lda 3,x
+    cmp 3,y
+    bcc :+
+    bne :+
+    lda 2,x
+    cmp 2,y
+    bcc :+
+    bne :+
+    lda 1,x
+    cmp 1,y
+    bcc :+
+    bne :+
+    lda 0,x
+    cmp 0,y
+:   rts
 .endproc
 
 .proc zpd_inc_x

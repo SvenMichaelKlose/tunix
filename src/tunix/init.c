17d9d38f4db923336f31483c284bdd14ff6ae310
Sven Michael Klose <pixel@hugbox.org>
2024-04-17 12:01:08 +0200

    Add extended memory bank management tests.

--- src/tunix/init.c
+++ src/tunix/init.c
@@ -10,6 +10,53 @@
 #define MAX_PROCS   64
 
 char processes[MAX_PROCS];
+char banks[256];
+
+char
+test_alloc0 (char phase)
+{
+    int i, mi;
+    char bank;
+
+    printf ("### (Round %d.)\n",
+            phase);
+
+    printf ("#### Allocating.\n");
+    for (i = 0; i < 156; i++) {
+        bank = tunix_alloc ();
+        if (!bank) {
+            printf ("\n%d banks allocated.\n", i);
+            break;
+        }
+        banks[i] = bank;
+        printf ("$%02x ", bank);
+    }
+
+    printf ("#### Freeing.\n");
+    for (mi = i, i = 0; i < mi; i++) {
+        bank = banks[i];
+        printf ("$%02x ", bank);
+        tunix_free (bank);
+    }
+
+    printf ("\n### Done.\n");
+    return mi;
+}
+
+void
+test_alloc (void)
+{
+    char nbanks_a = test_alloc0 (1);
+    char nbanks_b = test_alloc0 (2);
+    if (nbanks_a != nbanks_b) {
+        printf ("! Missing %d banks ",
+                "to allocate in second "
+                "round.\n",
+                nbanks_a - nbanks_b);
+        tunix_exit (-1);
+    }
+    printf ("Test passed.\n");
+}
 
 char
 make_baby ()
@@ -32,12 +79,12 @@ test_fork (char nprocs)
 {
     char i, mi, pid;
 
-    printf ("*** %d procs at once.\n",
+    printf ("## %d procs at once.\n",
             nprocs);
 
     for (i = 0; i < nprocs; i++) {
         tunix_mode (0);
-        printf ("Forking #%d.\n", i + 1);
+        printf ("Forking %d.\n", i + 1);
         tunix_mode (1);
         //if (nprocs == 1 && i == 0)
             //debug ();
@@ -60,7 +107,7 @@ test_fork (char nprocs)
         printf ("$%02x exited.\n", pid);
         tunix_mode (1);
     }
-    printf ("***\n");
+    printf ("\n");
 }
 
 void
@@ -68,7 +115,9 @@ main (void)
 {
     char nprocs;
 
-    printf ("Doing userland test!\n");
+    printf ("# Doing userland test!\n");
+
+    test_alloc ();
     for (nprocs = 1;
          nprocs < 3;
          nprocs++)

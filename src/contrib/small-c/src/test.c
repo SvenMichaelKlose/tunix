4fdc986b2573e1bc37aeb13c9248af2bade0f0d2
Sven Michael Klose <pixel@hugbox.org>
2024-04-20 06:39:18 +0200

    Add more test code.

--- src/contrib/small-c/src/test.c
+++ src/contrib/small-c/src/test.c
@@ -1,11 +1,19 @@
 // Just some code to test new targets.
 
-char array[5 + 3];
+char array[5];
+char i = 1;
+
+add (char a, char b)
+{
+    return a + b;
+}
 
 main (char argc, char *argv[])
 {
+    char * welcome;
     char i;
     char *p;
+    welcome = "Hello World!";
     p = 0x1000;
     for (i = 0; i < 500; i++)
         p[i] = 23;

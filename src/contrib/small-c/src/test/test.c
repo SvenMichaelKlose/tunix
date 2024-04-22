13d0bdfc1d101d349f5e302ea4473d46106b6e21
Sven Michael Klose <pixel@hugbox.org>
2024-04-22 04:17:24 +0200

    Try a struct.

--- src/contrib/small-c/src/test/test.c
+++ src/contrib/small-c/src/test/test.c
@@ -1,6 +1,15 @@
 // Just some code to test new targets.
 
+struct btnode {
+    char parent;
+    char left;
+    char right;
+    char data;
+};
+
 char array[5];
+
+struct btnode tree[256];
 
 main ()
 {

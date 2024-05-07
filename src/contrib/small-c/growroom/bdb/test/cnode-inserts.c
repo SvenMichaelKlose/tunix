aedb4e9829c177aff3443366686e009b0c212df4
Sven Michael Klose <pixel@hugbox.org>
2024-05-07 13:40:42 +0200

    Clean up.

--- src/contrib/small-c/growroom/bdb/test/cnode-inserts.c
+++ src/contrib/small-c/growroom/bdb/test/cnode-inserts.c
@@ -44,45 +44,34 @@ data2key (void *rec)
 void
 test_cnode_inserts (void)
 {
-    bdb *db    = &testdb;
+    bdb   *db;
     cnode *root;
     cnode *child1;
     cnode *child2;
 
-    printf ("! Adding ID index root node.\n");
     testdb.compare  = compare;
     testdb.data2key = data2key;
+    db = &testdb;
+
     root = make_int_cnode (1, 0);
     cache_insert_id (db, root);
-    TEST_ASSERT_MESSAGE(root == db->cache_root_ids,
-                        "New cnode not root of ID index.");
+    TEST_ASSERT(root == db->cache_root_ids);
 
-    printf ("! Adding first child.\n");
     child1 = make_int_cnode (2, 1);
     cache_insert_id (db, child1);
-    TEST_ASSERT_MESSAGE(root->iright == NULL,
-                        "Unexpected left child of ID root.");
-    TEST_ASSERT_MESSAGE(root->ileft == child1,
-                        "First should be left child of ID root.");
+    TEST_ASSERT(root->iright == NULL);
+    TEST_ASSERT(root->ileft == child1);
 
-    printf ("! Adding second child.\n");
     child2 = make_int_cnode (3, 2);
     cache_insert_id (db, child2);
-    TEST_ASSERT_MESSAGE(child1->iright == NULL,
-                        "Unexpected right child of first.");
-    TEST_ASSERT_MESSAGE(child1->ileft == child2,
-                        "Second isn't left child of ID root.");
+    TEST_ASSERT(child1->iright == NULL);
+    TEST_ASSERT(child1->ileft == child2);
 
-    printf ("! Removing first child.\n");
     cache_index_remove_id (db, child1);
-    TEST_ASSERT_MESSAGE(db->cache_root_ids,
-                        "Root node missing.");
-    TEST_ASSERT_MESSAGE(db->cache_root_ids == root,
-                        "Root node changed.");
-    TEST_ASSERT_MESSAGE(root->iright == NULL,
-                        "Unexpected left child of ID root.");
-    TEST_ASSERT_MESSAGE(root->ileft == child2,
-                        "Second isn't left of ID root.");
+    TEST_ASSERT(db->cache_root_ids);
+    TEST_ASSERT(db->cache_root_ids == root);
+    TEST_ASSERT(root->iright == NULL);
+    TEST_ASSERT(root->ileft == child2);
 }
 
 void setUp (void) {}

8ca31a1a119b661a1197b0fc176c2ae1767d33cc
Sven Michael Klose <pixel@hugbox.org>
2024-05-08 15:43:46 +0200

    Add oscar64.

--- src/sys/kernel/tunix.c
+++ src/sys/kernel/tunix.c
@@ -5,12 +5,30 @@ typedef unsigned char uchar;
 typedef uchar procid_t;
 typedef uchar waitid_t;
 typedef uchar fn_t;
+typedef uchar bankid_t;
+typedef uchar bankref_t;
+typedef uchar driverid_t;
+typedef uchar sighdl_t;
+typedef uchar sigid_t;
+
+#define MAX_FN          256
+#define MAX_BANK        128
+#define MAX_DRIVER      16
+#define MAX_WAITING     16
+#define MAX_DEV         32
+#define MAX_HANDLERS    256
+#define MAX_PROCS       32
+
+#define ERROR   -1
+#define OK      0
 
 struct fn {
     fn_t next;
     fn_t prev;
 };
 struct fn gfns[MAX_FN];
+
+uchar glfn_refs[MAX_FN];
 
 struct bank {
     fn_t      next;
@@ -19,6 +37,8 @@ struct bank {
 };
 struct bank banks[MAX_BANK];
 
+bankref_t bank_ref[MAX_BANK];
+
 struct driver {
     driverid_t  next;
     driverid_t  prev;
@@ -26,6 +46,10 @@ struct driver {
     void *      vectors[32];
 };
 struct driver drivers[MAX_DRIVER];
+
+struct signal {
+    char todo;
+};
 
 struct proc {
     uchar     flags;
@@ -38,6 +62,7 @@ struct proc {
     } waiting[MAX_WAITING];
     waitid_t       first_waiting;
     struct bank    lbanks[MAX_BANK];
+    bankref_t      lbank_ref[MAX_BANK];
     struct fn      lfns[MAX_FN];
     fn_t           lfn_glfn[MAX_FN];
     driverid_t     devices[MAX_DEV];
@@ -49,6 +74,18 @@ struct proc {
 
 procid_t pid;
 
+#define PROC_ZOMBIE     0x10
+#define PROC_RUNNING    0x40
+#define PROC_SLEEPING   0x80
+
+#define IS_RUNNING(x) (procs[x].flags | PROC_RUNNING)
+#define IS_SLEEPING(x) (procs[x].flags | PROC_SLEEPING)
+
+#define ALLOC_PROC_RUNNING() 0
+
+#define MV_SLEEPING_RUNNING(x)
+#define MV_RUNNING_SLEEPING(x)
+
 ///////////////////
 // Process lists //
 ///////////////////
@@ -58,9 +95,32 @@ procid_t sleeping;
 procid_t zombie;
 
 void
+machdep_switch (procid_t id)
+{
+    (void) id;
+}
+
+void
+machdep_fork (procid_t id)
+{
+    (void) id;
+}
+
+void
+update_procdata (procid_t id)
+{
+    (void) id;
+}
+
+void
+machdep_fork_init_child (void)
+{
+}
+
+void
 schedule ()
 {
-    procid_t next = proc->next;
+    procid_t next = procs[pid].next;
     if (!next
         && (next = running)
         && !IS_RUNNING(next))
@@ -74,17 +134,17 @@ schedule ()
 char
 fork (void)
 {
-    char cpid = ALLOC_PROC_RUNNING();
+    char cpid = ALLOC_PROC_RUNNING();;
     if (!cpid)
-        return -1;
+        return ERROR;
     machdep_fork (cpid);
     if (pid != cpid) {
-        update_procdata (cid);
+        update_procdata (cpid);
         procs[cpid].flags = PROC_RUNNING;
         return cpid;
     }
     machdep_fork_init_child ();
-    return 0;
+    return OK;
 }
 
 int
@@ -106,82 +166,112 @@ resume (char x)
 }
 
 void
-free_proc_resources (procid_t id)
+free_drivers (procid_t pid)
 {
-    free_lfns (id);
-    free_drivers (id);
-    free_iopages (id);
-    free_lbanks (id);
+    (void) pid;
 }
+
+void
+free_iopages (procid_t pid)
+{
+    (void) pid;
+}
+
+void
+free_lbanks (procid_t pid)
+{
+    (void) pid;
+}
+
+void
+panic (void)
+{
+}
+
+void
+free_proc_resources (procid_t pid)
+{
+    (void) pid;
+}
+
+#define DRM(x, y, z)
+#define LPUSH(x, y, z)
 
 void
 zombify (procid_t id)
 {
-    if (!proc[id]->flags)
+    if (!procs[id].flags)
         panic ();
     free_proc_resources (id);
-    if (PROC_IS_RUNNING(id))
+    if (IS_RUNNING(id))
         DRM(procs, running, id);
     else
         DRM(procs, sleeping, id);
     LPUSH(procs, zombie, id);
-    proc[id].flags = PROC_ZOMBIE;
+    procs[id].flags = PROC_ZOMBIE;
 }
 
 void
 resume_waiting (procid_t id)
 {
-    char w = procs[id].first_wait;
+    char w = procs[id].first_waiting;
     if (w)
         resume (w);
 }
+
+#define LPOP(x, y, z) 0
 
 char
 lfn_to_glfn (fn_t x)
 {
     fn_t glfn;
-    if (glfn = lfn_glfn[x])
+    if (glfn = procs[pid].lfn_glfn[x])
         return glfn;
-    LPUSH(lfns, first_lfn, x);
-    LPOP(glfn, glfns, free_glfn);
+    LPUSH(lfns, first_lfn, x);;
+    LPOP(glfn, glfns, free_glfn);;
     glfn_refs[glfn]++;
-    return lfn_glfn[x] = glfn;
+    procs[pid].lfn_glfn[x] = glfn;
+    return glfn;
 }
 
 void
-free_lfns (void)
+free_lfns (procid_t pid)
 {
     fn_t glfn;
     fn_t y;
-    DOLIST(y, lfns, first_lfn) {
-        glfn = lfn_glfn[y];
+/*
+    DOLIST(y, procs[pid].lfns, procs[pid].first_lfn) {
+        glfn = procs[pid].lfn_glfn[y];
         if (!--glfn_refs[glfn])
-            LPUSH(glfns, first_glfn, glfn);
+            LPUSH(glfns, procs[pid].first_glfn, glfn);
     }
+*/
 }
 
-bank_t
+#define DPUSH(x, y)
+
+bankid_t
 balloc (void)
 {
-    char b = LPOP(banks);
+    char b = 0; //LPOP(banks);
     if (b) {
-        DPUSH(lbanks, first_lbank);
-        bank_refs[b]++;
+        DPUSH(procs[pid].lbanks, procs[pid].first_lbank);
+        bank_ref[b]++;
         return b;
     }
     return 0;
 }
 
 void
-free_lbank (bank_t b)
+free_lbank (bankid_t b)
 {
-    DRM(lbanks, first_bank);
+    //DRM(procs[pid].lbanks, procs[pid].first_bank);
 }
 
 int
-bfree (bank_t b)
+bfree (bankid_t b)
 {
-    if (!lbanks_ref[b])
+    if (!procs[pid].lbank_ref[b])
         return ERROR;
     free_lbank (b);
     if (!bank_ref[b])
@@ -195,6 +285,13 @@ void
 bprocfree (void)
 {
     char i;
-    DOLIST(i, lbanks, first_lbank)
+    //DOLIST(i, procs[pid].lbanks, procs[pid].first_lbank)
         bfree (i);
+}
+
+int
+main (void)
+{
+    fork ();
+    return 0;
 }

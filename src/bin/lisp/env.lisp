22f061b6dbc7f121c29c6b7874f9ea9f3187f629
Sven Michael Klose <pixel@hugbox.org>
2024-06-05 22:32:34 +0200

    Remove sketches.

--- src/bin/lisp/env.lisp
+++ src/bin/lisp/env.lisp
@@ -4,6 +4,10 @@
 (fn list x
   x)
 
+(fn list? (x)
+  (or (not x)
+      (cons? x)))
+
 (fn caar (x)
   (car (car x)))
 
@@ -17,11 +21,11 @@
   (cdr (cdr x)))
 
 (fn member (n x)
-  (? x
-     (or (and (eq n (car x)) x)
-         (member n (cdr x)))))
-
-(var *macros* nil)
+  (? (list? x)
+     (or (and (eq n (car x))
+              x)
+         (member n (cdr x)))
+     (error)))
 
 (fn find (x l)
   (and (cons? l)
@@ -29,35 +33,16 @@
           x
           (find x (cdr l)))))
 
-(fn macro? (s)
-  (find s *macros*))
-
-(fn macroexpand-bq (x)
-  x)
-
-(fn macroexpand (x)
-  (? (cons? x)
-     (?
-       (eq (car x) 'quote)
-         x
-       (eq (car x) 'backquote)
-         (macroexpand-bq x)
-       (@ macroexpand 
-          (? (macro? (car x))
-             (macroexpand (apply (car x) (cdr x)))
-             x)))
-     x))
-
 (fn assoc (v x)
   (? (cons? x)
      (? (cons? (car x))
         (? (eq v (caar x))
-           (cdar x)
+           x
            (assoc v (cdr x)))
         (error))))
 
 (fn make-count (n)
-  (? (not (== 0 n))
+  (? (< 0 n)
      (cons n (make-count (-- n)))))
 
 (fn block-test (c)
@@ -72,53 +57,5 @@
        (print c))
     (? (not (== c 0))
        (go tag))))
-
-(fn progn body
-  ^(block t
-     ,@body))
-
-(fn let (n v . body)
-  (((g)
-     ^(((,n)
-         ,@body
-       ,v)))
-   (symbol)))
-
-(fn prog1 body
-  (with (g (symbol))
-    ^(((,g)
-        ,@(cdr body)
-        ,g)
-      (car body))))
-
-(fn when (x . body)
-  ^(? ,x
-      (progn
-        ,@body)))
-
-(fn unless (x . body)
-  ^(? (not ,x)
-      (progn
-        ,@body)))
-
-(fn while (cond result . body)
-  (let tag (symbol)
-    ^(block nil
-       (or ,cond
-           (return))
-       ,@body)))
-
-(fn dolist (iter . body)
-  (with (v   (car iter)
-         i   (cadr iter)
-         r   (cddr iter)
-         tag (symbol))
-    ^(let ,v ,i
-       (block nil
-         (or ,v (return ,(car r)))
-         ,tag
-         ,body
-         (= ,v (cdr v))
-         (go ,tag)))))
 
 (print (gc))(out " bytes free.")(terpri)

6a9861390cbb8f372c90482d29ae60497021047b
Sven Michael Klose <pixel@hugbox.org>
2024-06-09 16:52:53 +0200

    Trigger GC if stack is almost filled.

--- src/lib/lisp/heap.c
+++ src/lib/lisp/heap.c
@@ -85,7 +85,8 @@ objsize (char * x)
 #define MIN_RELOC_TABLE_SIZE \
     ((sizeof (lispptr) + sizeof (unsigned)) * MIN_RELOC_TABLE_ENTRIES)
 #define NEEDS_GC() \
-    (heap_free > heap_end - size - MIN_RELOC_TABLE_SIZE)
+    ((heap_free > heap_end - size - MIN_RELOC_TABLE_SIZE) \
+     || (stack_start > stack - 64)) // TODO: Reconsider.
 
 // Allocate vanilla object.
 lispptr FASTCALL

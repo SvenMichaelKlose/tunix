e59dffcd779e090ca6628867f4a096d7c5e7fd79
Sven Michael Klose <pixel@hugbox.org>
2024-06-09 23:35:11 +0200

    Touch some built-ins.

--- src/bin/lisp/test.lisp
+++ src/bin/lisp/test.lisp
@@ -1,14 +1,48 @@
-; Essential tests before regular environment is loaded.
-
 (out "Tests.")(terpri)
+
+; Poke built-ins.
 (out "ATOM")(terpri)
 (atom 1)
 (out "OR")(terpri)
 (or nil t)
 (or t nil)
+(out "APPLY")(terpri)
+(print (apply print '(1)))(terpri)
+(out "FUNCALL")(terpri)
+(funcall print 1)(terpri)
+(funcall '((x) (print x)) 1)(terpri)
+(out "EVAL")(terpri)
+(eval '(print 1))
+(out "?")(terpri)
+(? t nil (error))
+(? nil (error) nil)
+(? nil (error) nil (error))
+(out "AND")(terpri)
+(and nil (error))
+(out "OR")(terpri)
+(or t (error))
+(out "NOT")(terpri)
+(or (not nil) (error))
+(out "ATOM")(terpri)
+(? (atom nil) nil (error))
+(? (atom t) nil (error))
+(out "CONS?")(terpri)
+(? (cons? '(1)) nil (error))
+(? (cons? 1) (error))
+(out "SYMBOL?")(terpri)
+(? (symbol? 'a) nil (error))
+(out "NUMBER?")(terpri)
+(? (number? 1) nil (error))
+(? (number? '(1)) (error))
 
-; TODO: Test rest of built-ins.
-
+; = value string cons car cdr setcar setcdr
+; == > < >= <=
+; + - * / % ++ --
+; bit-and bit-or bit-xor bit-neg << >>
+; peek poke sys
+; read print open err eof in out terpri fresh-line setin setout putback close load
+; fn var universe gc exit
+; length butlast last member @
 
 (fn make-count (n)
   (? (< 0 n)

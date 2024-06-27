3f4d21847c7549f86656365553bc53d96a110307
Sven Michael Klose <pixel@hugbox.org>
2024-06-27 02:59:12 +0200

    Sketch how to call built-in.
    And essentially other bytecodes by pushing arguments on the stack
    instead.

--- src/bin/lisp/growroom/bytecode-6502.asm
+++ src/bin/lisp/growroom/bytecode-6502.asm
@@ -27,16 +27,118 @@
     inc bcc+1
 :   sty bcc+1
 
-    ; Execute code
-l:  ldy #0
+    ; Get code.
+next:
+    ldy #0
     lda (bcc),y
     beq return
-    sta op
-    bmi jumps
+    inc bcc
+    bcc :+
+    inc bcc+1
+:
+    ; Get function object.
+    asl
+    tay
+    lda (bcp),y
+    sta fun
+    iny
+    lda (bcp),y
+    sta fun+1
 
-    ; Built-in
-    ; Bytecode
-    ; User
+    ; Get value of function.
+    ldy #IDX_SYMBOL_VALUE
+    lda (fun),y
+    sta value
+    iny
+    lda (fun),y
+    sta value
+
+    ;;;;;;;;;;;;;;;;
+    ;;; Built-in ;;;
+    ;;;;;;;;;;;;;;;;
+
+    cmp #TYPE_BUILTIN
+    bne no_builtin
+
+    ldx #0  ; args
+l:  ldy #0
+    lda (bcc),y
+    bmi bi_call
+    asl
+    bmi bi_const
+
+    ; Stack arg
+    tay
+    lda (sp),y
+    iny
+    sta args,x
+    inx
+    lda (sp),y
+    sta args,x
+    inx
+    inc bcc
+    bcc l
+    inc bcc+1
+    bne l   ; (jmp)
+
+    ; Constant list argument
+bi_const:
+    and #$7f
+    tay
+    lda (bcp),y
+    iny
+    sta args,x
+    inx
+    lda (bcp),y
+    sta args,x
+    inx
+    inc bcc
+    bcc l
+    inc bcc+1
+    bne l   ; (jmp)
+
+    ; Call built-in
+bi_call:
+    jsr push_bcp
+    jsr j
+    jsr pop_bcp
+
+set_place:
+    sta value
+
+    ; Get stack index.
+    ldy #0
+    lda (bcc),y
+    tay
+
+    inc bcc
+    bcc :+
+    inc bcc+1
+:
+    ; Copy value to stack.
+    lda value
+    sta (sp),y
+    iny
+    txa
+    sta (sp),y
+    jmp next
+
+j:  jmp (value)
+
+no_builtin:
+
+    ;;;;;;;;;;;;;;;;
+    ;;; Bytecode ;;;
+    ;;;;;;;;;;;;;;;;
+    ; Same as built-in but copying arguments to the GC
+    ; stack instead of 'args'..
+no_bytecode:
+
+    ;;;;;;;;;;;;
+    ;;; User ;;;
+    ;;;;;;;;;;;;
+    ; Get start of argument definition.
+    ; Save and set symbol values.
 
 jumps:
     asl

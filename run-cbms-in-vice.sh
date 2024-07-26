1f3a43cb4285b99c0ba37a1040641727749d6049
Sven Michael Klose <pixel@hugbox.org>
2024-07-26 15:53:45 +0200

    Break on error.

--- run-cbms-in-vice.sh
+++ run-cbms-in-vice.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 make clean all TARGET=c128 && x128 -moncommands src/bin/lisp/lisp.lbl tunix-lisp.c128.d64
 make clean all TARGET=c16 && xplus4 -moncommands src/bin/lisp/lisp.lbl tunix-lisp.c16.d64
 make clean all TARGET=c64 && x64 -moncommands src/bin/lisp/lisp.lbl tunix-lisp.c64.d64

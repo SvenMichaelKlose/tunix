bf8b7f887f55a5a5a6170b2d49c5d084b859f489
Sven Michael Klose <pixel@hugbox.org>
2024-07-26 22:29:41 +0200

    Rename macro SET-OP ot DEFSETFN.

--- src/bin/lisp/set.lisp
+++ src/bin/lisp/set.lisp
@@ -11,29 +11,29 @@
      l
      (cons x l)))
 
-(macro set-op (name . body)
+(macro defsetfn (name . body)
   $(fn ,name (a b)
      ,@body ))
 
-(set-op intersect
+(defsetfn intersect
   ;"Elements that are in both lists."
   (and a b
        (? (member (car a) b)
           (cons (car a) (intersect (cdr a) b))
           (intersect (cdr a) b))))
 
-(set-op set-difference
+(defsetfn set-difference
   ;"Elements in list b that are not in list a."
   (and b
        (? (member (car b) a)
           (set-difference a (cdr b))
           (cons (car b) (set-difference a (cdr b))))))
 
-(set-op union
+(defsetfn union
   ;"Unique elements from both lists."
   (unique (append a b)))
 
-(set-op set-exclusive-or
+(defsetfn set-exclusive-or
   ;"Elements that are not in both lists."
   (with ((subset  (intersect a b))
          (pred    $((x)
@@ -41,6 +41,6 @@
     (append (remove-if pred a)
             (remove-if pred b))))
 
-(set-op subseq?
+(defsetfn subseq?
   ;"Check if list a is a subset of list b."
   (every $((x) (member x ,a)) b))

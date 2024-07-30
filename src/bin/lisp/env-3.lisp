938c5d56466fa91318babbb6826242b400b5e65e
Sven Michael Klose <pixel@hugbox.org>
2024-07-30 14:51:30 +0200

    Load prerequisites for DO.

--- src/bin/lisp/env-3.lisp
+++ src/bin/lisp/env-3.lisp
@@ -1,13 +1,16 @@
+(load "progn.lisp")
+(load "let.lisp")
+(load "with.lisp")
 (load "do.lisp")
 (terpri)
 (message "Welcome to TUNIX Lisp!")
-(out "Left to LOAD: ")
+(out "LOADables: (Append \".lisp\"!)")(terpri)
 (print '(adjoin alist dolist dotimes ensure-list group
          intersect let max nthcdr prog1 queue queue-pop
          set-difference set-exclusive-or source stack
          source subseq subseqp union unique unless when
          while with-queue))
 (terpri)
-(message "(Append \".lisp\" to filenames.)")
 (print (gc))(out "B free.")(terpri)
 (message "Ready.")
+(load member-if.lisp)

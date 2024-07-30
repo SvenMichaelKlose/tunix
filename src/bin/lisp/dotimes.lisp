71098a8780af7698900e9075333e8597941d7c35
Sven Michael Klose <pixel@hugbox.org>
2024-07-30 14:30:34 +0200

    Passing test of DOTIMES.

--- src/bin/lisp/dotimes.lisp
+++ src/bin/lisp/dotimes.lisp
@@ -1,9 +1,9 @@
 (macro dotimes (init . body)
   (with ((i (car init))
          (n (symbol)))
-    $(let ,n (cadr init)
+    $(let ,n ,(cadr init)
        (do ((,i 0 (+ ,i 1)))
-           ((< ,i ,n) ,@(cddr init))
+           ((>= ,i ,n) ,@(cddr init))
          ,@body))))
 
 (message "Testing DOTIMES...")

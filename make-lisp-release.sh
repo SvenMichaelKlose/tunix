43de96de50721d3a144e81a7afc2878b247f1da1
Sven Michael Klose <pixel@hugbox.org>
2024-08-02 09:42:21 +0200

    Take additional command-line arguments for 'make'.

--- make-lisp-release.sh
+++ make-lisp-release.sh
@@ -3,7 +3,7 @@
 set -e
 
 DATE=`date +%Y-%m-%d`
-make allworlds NDEBUG=1
+make allworlds NDEBUG=1 $@
 cd src/bin/lisp/doc && ./md2pdf.sh && cd -
 cp src/bin/lisp/doc/manual.pdf tunix-lisp.pdf
 cp src/bin/lisp/doc/manual.md tunix-lisp.md

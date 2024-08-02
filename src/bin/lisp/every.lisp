bf07a2d64d422247927652f37a4ad41d44c7758c
Sven Michael Klose <pixel@hugbox.org>
2024-08-02 11:50:27 +0200

    Add test.

--- src/bin/lisp/every.lisp
+++ src/bin/lisp/every.lisp
@@ -2,5 +2,11 @@
   "F true for all in X?"
   (do ((i x (cdr i)))
       ((not i) t)
-    (or (f i)
-        (return))))
+    (or (f (car i))
+        (return nil))))
+
+(message "Testing EVERY...")
+(or (every '((x) (== x 1)) '(1 1 1 1))
+    (error))
+(and (every '((x) (== x 1)) '(1 1 2 1))
+    (error))

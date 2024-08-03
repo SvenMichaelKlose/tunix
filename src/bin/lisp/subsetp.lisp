55c0d2075b6046b9fb028ff709e288b30bdf4ba7
Sven Michael Klose <pixel@hugbox.org>
2024-08-03 22:29:29 +0200

    Remove SUBSET?.
    Assuming that it won't be needed anytime soon.

--- src/bin/lisp/subsetp.lisp
+++
@@ -1,8 +0,0 @@
-(or (macro? 'defsetfn)
-    (load "defsetfn.lisp"))
-(or (cons? every)
-    (load "every.lisp"))
-
-(defsetfn subset?
-  ;"Check if list a is a subset of list b."
-  (every $((x) (member x ,a)) b))

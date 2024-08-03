8b1217cc5f065f00d71297ef577040a54b2ca36c
Sven Michael Klose <pixel@hugbox.org>
2024-08-03 23:52:18 +0200

    Include the Commodore PET version although it won't start.
    Perhaps someone has an idea why it's broken.

--- make-lisp-release.sh
+++ make-lisp-release.sh
@@ -7,7 +7,6 @@ make allworlds NDEBUG=1 $@
 cd src/bin/lisp/doc && ./md2pdf.sh && cd -
 cp src/bin/lisp/doc/manual.pdf tunix-lisp.pdf
 cp src/bin/lisp/doc/manual.md tunix-lisp.md
-rm tunix-lisp.pet.d64
 rm tunix-lisp.unix.d64
 rm -f tunix.cbm.$DATE.zip
 zip -o tunix.cbm.$DATE.zip tunix-lisp.md tunix-lisp.pdf *.d64

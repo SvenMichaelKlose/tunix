25eec2c1d71d8c4ea8e2e28e1e339ab55209d25d
Sven Michael Klose <pixel@hugbox.org>
2024-08-06 12:13:48 +0200

    Do not try to use cc65 label file.
    It's for the most recent target built.

--- run-cbms-in-vice.sh
+++ run-cbms-in-vice.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-xvic -moncommands src/bin/lisp/lisp.lbl tunix-lisp.vic20.d64
-x64 -moncommands src/bin/lisp/lisp.lbl tunix-lisp.c64.d64
-x128 -moncommands src/bin/lisp/lisp.lbl tunix-lisp.c128.d64
-xplus4 -moncommands src/bin/lisp/lisp.lbl tunix-lisp.c16.d64
-xplus4 -moncommands src/bin/lisp/lisp.lbl tunix-lisp.plus4.d64
+xvic tunix-lisp.vic20.d64
+x64 tunix-lisp.c64.d64
+x128 tunix-lisp.c128.d64
+xplus4 tunix-lisp.c16.d64
+xplus4 tunix-lisp.plus4.d64

6aa731d51b15dbca43fbcd5c06b0ddf82c516a06
Sven Michael Klose <pixel@hugbox.org>
2024-08-06 09:42:57 +0200

    Do not pass compile-time options by default and rename script accordingly.

---
+++ make-lisp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+DATE=`date +%Y-%m-%d`
+make allworlds $@
+cd src/bin/lisp/doc && ./md2pdf.sh && cd -
+cp src/bin/lisp/doc/manual.pdf tunix-lisp.pdf
+cp src/bin/lisp/doc/manual.md tunix-lisp.md
+rm tunix-lisp.unix.d64
+rm -f tunix.cbm.$DATE.zip
+zip -o tunix.cbm.$DATE.zip tunix-lisp.md tunix-lisp.pdf *.d64

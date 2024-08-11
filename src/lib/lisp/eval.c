ef9093a6397a6a542f5e2337ded35832c4291ce3
Sven Michael Klose <pixel@hugbox.org>
2024-08-11 22:21:23 +0200

    init_eval(): Clear 'argdefs'.
    Only for letting GC_STRESS pass on 6502s, where it is placed on the
    zeropage.  GC is triggered with the first allocation before 'argdefs'
    is actually used and check_lispptr() complains then.

--- src/lib/lisp/eval.c
+++ src/lib/lisp/eval.c
@@ -784,7 +784,7 @@ init_eval ()
 {
     // TODO: Clear zeropage instantly in a loop instead.
     go_tag = return_name = return_value = nil;
-    args = arg1 = arg2 = arg2c = x = value = va = nil;
+    args = argdefs = arg1 = arg2 = arg2c = x = value = va = nil;
     return_sym   = make_symbol (NULL, 0);
     go_sym       = make_symbol (NULL, 0);
     delayed_eval = make_symbol (NULL, 0);

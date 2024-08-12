7f3d2f5dd7cf9f3cd2411cf7c0d37e8e54aaee37
Sven Michael Klose <pixel@hugbox.org>
2024-08-13 00:14:19 +0200

    Fix what I type when I was tired blind.

--- src/bin/lisp/aif.lisp
+++ src/bin/lisp/aif.lisp
@@ -1,9 +1,9 @@
-(macro !? (c &body x)
+(macro !? (c . x)
   ;"Like '?' but assigning result of condition to '!'.
   (? x
      $(let ! ,c
         (? !
            ,(car x)
            ,@(? (cdr x)
-                !((%!? ,@(cdr x))))))
+                $((%!? ,@(cdr x))))))
      c))

f581ddb95213ec513245755a580a6c3db8fe9374
Sven Michael Klose <pixel@hugbox.org>
2024-08-13 19:46:33 +0200

    Make all worlds and stress out.

--- test-unix.sh
+++ test-unix.sh
@@ -4,8 +4,8 @@ set -e
 
 build_and_run () {
     echo "#### Testing $@"
-    #make clean allworlds $@
-    make clean all TARGET=unix LOAD_ALL=1
+    make clean allworlds $@
+    make clean all TARGET=unix LOAD_ALL=1 $@
     cd tunix/unix/ && (echo | ./lisp) ; cd -
 }
 
@@ -28,14 +28,17 @@ test_diagnostics () {
 
 test_diagnostics
 
-# Stealth version
+# Stealth version.
 build_and_run NAIVE=1 $@
 build_and_run NAIVE=1 COMPRESSED_CONS=1 $@
 
-
-# Release version
-build_and_run VERBOSE_LOAD=1 COMPRESSED_CONS=1 NDEBUG=1 $@
-
-# Test image
+# Test image and be paranoid about it.
+build_and_run VERBOSE_LOAD=1 COMPRESSED_CONS=1 VERBOSE_COMPRESSED_CONS=1 PARANOID=1 CHECK_OBJ_POINTERS=1 $@
 cd tunix/unix/ && (echo "(isave \"image\")" | ./lisp) ; cd -
 cd tunix/unix/ && (echo | ./lisp) ; cd -
+
+# Stress out.
+build_and_run VERBOSE_LOAD=1 COMPRESSED_CONS=1 VERBOSE_COMPRESSED_CONS=1 PARANOID=1 CHECK_OBJ_POINTERS=1 GC_STRESS=1 $@
+
+# Release version.
+build_and_run VERBOSE_LOAD=1 COMPRESSED_CONS=1 NDEBUG=1 $@

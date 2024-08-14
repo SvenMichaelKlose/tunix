c09a387a12d1ebbcaf7cc3727015ee3f2d5d45ee
Sven Michael Klose <pixel@hugbox.org>
2024-08-14 14:51:40 +0200

    Rename file to something more adequate.

--- make-bin-release.sh
+++ make-bin-release.sh
@@ -3,7 +3,7 @@
 set -e
 
 DATE=`date +%Y-%m-%d`
-make allworlds $@
+make allworlds VERBOSE_LOAD=1 COMPRESSED_CONS=1 NDEBUG=1 $@
 cd src/bin/lisp/doc && ./md2pdf.sh && cd -
 cp README.md tunix/
 cp src/bin/lisp/doc/manual.md tunix/tunix-lisp.md

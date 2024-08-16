18b35c874cf164603f81d217660d3cba69c9d922
Sven Michael Klose <pixel@hugbox.org>
2024-08-16 07:41:53 +0200

    Add PDF manual to ZIP again. Use script to make ZIP.

--- make-bin-release.sh
+++ make-bin-release.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-DATE=`date +%Y-%m-%d`
-make allworlds LISP_FLAGS="-DNDEBUG" $@
+make allworlds LISP_FLAGS="-DNDEBUG"
 cd src/bin/lisp/doc && ./md2pdf.sh && cd -
 cp README.md tunix/
+cp src/bin/lisp/doc/manual.pdf tunix/tunix-lisp.pdf
 cp src/bin/lisp/doc/manual.md tunix/tunix-lisp.md
-cp src/bin/lisp/doc/manual.md tunix/tunix-lisp.md
-rm -f tunix.$DATE.zip
-zip -r -o tunix.$DATE.zip tunix
+rm -f $1
+zip -r -o $1 tunix

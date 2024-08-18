8da4192f35835c9bfee4e5e1ef6676959da43ca0
Sven Michael Klose <pixel@hugbox.org>
2024-08-18 02:55:08 +0200

    Sketch screen editor.

--- src/bin/lisp/editline.lisp
+++ src/bin/lisp/editline.lisp
@@ -1,8 +1,17 @@
-; Line editor
 (or (cons? subseq)
     (load "subseq.lisp"))
 
 (var *con-width* 22)
+(var *con-height* 23)
+
+;;; State
+
+(var saved? nil)
+(var lx 0)      ; Line X position.
+(var ln 0)
+(var conln 0)   ; First ln on console.
+
+;;; Console basics
 
 (fn con-cr ()
   (out 13)   ; CR+LF
@@ -16,13 +25,19 @@
   (out 1)
   (out 1))
 
-(var cx 0)
-
-(fn display-line (line)
+(fn update-line (line)
   (con-cr)
   (@ out (subseq line 0 *con-width*))
   (= cx (length line))
   (con-clr2eol))
+
+; Update screen from ln line on.
+; Scroll if necessary.
+(fn display ())
+
+(fn status (msg))
+
+;;; Editing
 
 (fn editline (x)
   (with ((line (symbol-name x)))
@@ -38,15 +53,80 @@
             (? (< cx len)
                (= cx (++ cx)))
           +del+
-            (? (< 0 cx)
+            (when (< 0 cx)
+              (= saved? nil)
               (= line (append (subseq line 0 (-- cx))
                               (subseq line cx))))
           (< c \ )
             (progn
-              (or (== c +enter+)
-                  (conputback))
+              (conputback)
               (return (symbol line)))
           t
-            (= line (append (subseq line 0 cx)
-                            (list c)
-                            (subseq line (++ cx)))))))))
+            (progn
+              (= saved? nil)
+              (= line (append (subseq line 0 cx)
+                              (list c)
+                              (subseq line (++ cx))))))))))
+
+(var lines nil)
+
+(fn read-lines (x)
+  (with-input (open x 'r)
+    (with-queue q
+      (while (not (eof))
+        (enqueue q (read-line))))))
+
+(fn save-file ()
+  (put-file x lines)
+  (? (err)
+     (status "Cannot save.")
+     (= saved? t)))
+
+(fn del-line (ln)
+  (= file (append (subseq lines 0 (-- ln))
+                  (subseq lines (++ ln)))))
+
+(fn choose x
+  (while t
+    (!= (conin)
+      (and (member ! x)
+           (return !)))))
+
+(fn quit-editor ()
+  (or modified?
+      (return t))
+  (status "Save first (y/n/c)?")
+  (!= (choose \y \n \c)
+    (and (== ! \y)
+         (save-file))
+    (not (== ! \c))))
+
+(fn edit-lines ()
+  (while t
+    (status fname)
+    (display)
+    (let lcons (nth file ln)
+      (!? (edit-line (car lcons))
+          (setcar lcons newl)
+          (del-line ln)))
+    (case (conin)
+      +arr-up+
+        (? (< 0 ln)
+           (= ln (-- ln)))
+      +arr-down+
+        (? (< ln (length file))
+           (= ln (++ ln)))
+      +hotkey+
+        (case (conin)
+          \s (save-file)
+          \q (and (quit-editor)
+                  (return))
+
+      t
+        (conputback))))
+
+(fn edit (file)
+  (= lines (read-lines file))
+  (= saved? t)
+  (edit-lines)
+  (out "Bye!")(terpri))

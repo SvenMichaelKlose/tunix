f4c673a6473f2074e5dd875d5eef4b9df4c94c06
Sven Michael Klose <pixel@hugbox.org>
2024-08-19 01:58:57 +0200

    Verbose LOAD and definitions.
    Better to stay tuned.

--- make-bin-release.sh
+++ make-bin-release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-make allworlds NDEBUG=1
+make allworlds NDEBUG=1 LISP_FLAGS="-DVERBOSE_LOAD=1 -DVERBOSE_DEFINES=1"
 cd src/bin/lisp/doc && ./md2pdf.sh && cd -
 cp src/bin/lisp/doc/manual.pdf tunix/tunix-lisp.pdf
 cp src/bin/lisp/doc/manual.md tunix/tunix-lisp.md

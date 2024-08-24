d2c9df891e58436efadc6dcf57950fcf20c1e054
Sven Michael Klose <pixel@hugbox.org>
2024-08-24 15:11:29 +0200

    Use 'pushd' and 'popd' instead of 'cd'.

--- scripts/run-cbms-in-vice.sh
+++ scripts/run-cbms-in-vice.sh
@@ -2,8 +2,22 @@
 
 set -e
 
-cd tunix/vic20 && xvic -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp; cd -
-cd tunix/c64 && x64 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp; cd -
-cd tunix/c128 && x128 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp; cd -
-cd tunix/c16 && xplus4 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp; cd -
-cd tunix/plus4 && xplus4 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp; cd -
+pushd tunix/vic20
+xvic -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp
+popd
+
+pushd tunix/c64
+x64 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp
+popd
+
+pushd tunix/c128
+x128 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp
+popd
+
+pushd tunix/c16
+xplus4 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp
+popd
+
+pushd tunix/plus4
+xplus4 -attach8rw -autostartprgmode 0 -moncommands lisp.lbl lisp
+popd

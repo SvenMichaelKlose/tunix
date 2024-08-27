d9f3624f542d7cc3c0f65122dda8e79bd6699041
Sven Michael Klose <pixel@hugbox.org>
2024-08-27 16:10:26 +0200

    Make PDF of compiler roadmap.

--- src/bin/lisp/doc/md2pdf.sh
+++ src/bin/lisp/doc/md2pdf.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 pandoc -V documentclass=report --from markdown --template eisvogel.tex --listings -o manual.pdf manual.md
+pandoc -V documentclass=report --from markdown --template eisvogel.tex --listings -o compiler.pdf compiler.md

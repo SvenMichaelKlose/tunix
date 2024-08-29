f6ef9a3ec21a7ba2da4db22b94480fe7de3c10c4
Sven Michael Klose <pixel@hugbox.org>
2024-08-29 14:03:54 +0200

    Remove user configs for editor (which is under construction).

--- src/bin/lisp/user-pre-image.lisp
+++
@@ -1,9 +0,0 @@
-(and (eq +vb+ "smk_editor")
-     (@ '((x)
-           (or (load (symbol (append (symbol-name x)
-                                     (symbol-name ".lisp"))))
-               (error "Can't load " x ".lisp")))
-        '("when" "unless" "dolist" "alet" "aif" "while" "awhile"
-          "queue" "with-queue" "dotimes" "incdec" "nth" "nthcdr"
-          "awhen" "nthcdr" "subseq" "mapcar" "mapcan" "case"
-          "group" "cbm-keycode" "cbm-con" "ls")))

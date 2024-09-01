fdd45cca21d4aa10147bbafd609af5e7ae6c6976
Sven Michael Klose <pixel@hugbox.org>
2024-09-01 21:19:13 +0200

    Define APPEND and SUBSEQ only if not already built-in.

--- src/bin/lisp/subseq.lisp
+++ src/bin/lisp/subseq.lisp
@@ -1,10 +1,11 @@
-(fn subseq (l start . end)
-  ;"Return the subsequence of LIST from START to END."
-  ;"If END is nil, the subsequence goes to the end of the list."
-  (with-queue q
-    (do ((e (car end))
-         (i start (!++ i))
-         (l (nthcdr start l) (cdr l)))
-        ((or (not l)
-             (and e (>= i e))))
-      (enqueue q (car l)))))
+(or (builtin? subseq)
+  (fn subseq (l start . end)
+    ;"Return the subsequence of LIST from START to END."
+    ;"If END is nil, the subsequence goes to the end of the list."
+    (with-queue q
+      (do ((e (car end))
+           (i start (!++ i))
+           (l (nthcdr start l) (cdr l)))
+          ((or (not l)
+               (and e (>= i e))))
+        (enqueue q (car l))))))

9d98f521b22d63ce0a0c25fb26ad556274038438
Sven Michael Klose <pixel@hugbox.org>
2024-10-11 11:54:44 +0200

    fix(interpreter): con_reset(): Don't clear screen, go home.

--- src/lib/lisp/con.c
+++ src/lib/lisp/con.c
@@ -5,21 +5,6 @@
 #include <simpleio/libsimpleio.h>
 #include <simpleio/control.h>
 
-char 
-con_reset (void)
-{   
-    char f;
-    out (TERM_CMD_GET);
-    f = conin ();
-    if (f & TERM_FLAG_DIRECT)
-        out (TERM_CMD_CLRSCR);
-    out (TERM_CMD_CLR);
-    out (255);
-    out (TERM_CMD_SET);
-    out (TERM_FLAG_CURSOR);
-    return f;
-}
-
 void
 con_set (char f)
 {   
@@ -27,4 +12,19 @@ con_set (char f)
     out (255);
     out (TERM_CMD_SET);
     out (f);
+}
+
+char
+con_reset (void)
+{
+    char f;
+    out (TERM_CMD_GET);
+    f = conin ();
+    if (f & TERM_FLAG_DIRECT) {
+        out (TERM_CMD_GOTO);
+        out (1);
+        out (1);
+    }
+    con_set (TERM_FLAG_CURSOR);
+    return f;
 }
